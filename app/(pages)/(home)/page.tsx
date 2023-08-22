40d1fb23bd8a7c24d2e252c977de43158c931dbf
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-22 18:14:14 +0300

    Minor changes

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { NextPage } from "next"
 
 let HomePage: NextPage = () => {
     return <div className=" h-screen bg-no-repeat mx-auto box-border max-w-10xl  w-full ">
+            
             <TypingAnimation />
         </div>
 }
