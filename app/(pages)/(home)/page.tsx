c8b9eaf010d6838b8903d226da5c3cb14e249f8f
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-23 23:30:04 +0300

    Praying on bug fixing(safari moment)

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -4,7 +4,7 @@ import HomeButtons from "@/app/components/HomeButtons/HomeButtons"
 import PageGradient from "@/app/UI/PageGradient"
 
 let HomePage: NextPage = () => {
-    return <div className=" h-screen bg-transparent mx-auto box-border max-w-10xl
+    return <div className=" overflow-hidden h-screen bg-transparent mx-auto box-border max-w-10xl
                             w-full pt-40 sm:pt-52 md:pt-64">
             <PageGradient/>                    
             <h1 className="text-4xl tracking-wider text-text sm:text-5xl
--- app/UI/PageGradient.tsx
+++ app/UI/PageGradient.tsx
@@ -1,8 +1,8 @@
 let PageGradient = () => {
     return (
         <div>
-        <div className=" w-10lg h-80 rotate-45 -skew-x-12 top-72 absolute -left-80 opacity-60  bg-gradient-to-r from-blue-700 to-blue-950 to- -z-2 blur-mega rounded-full"/>
-        <div className=" w-10lg h-80 rotate-12 -skew-x-12 top-96 absolute -right-60 opacity-60  bg-gradient-to-r from-blue-950 to-blue-700 to- -z-2 blur-mega rounded-full"/>
+        <div className=" w-10lg h-80 rotate-45 -skew-x-12 top-72 absolute -left-80 opacity-60 gradient -z-2 blur-mega rounded-full"/>
+        <div className=" w-10lg h-80 rotate-12 -skew-x-12 top-96 absolute -right-60 opacity-60 gradient -z-2 blur-mega rounded-full"/>
         </div>
     )
 }
