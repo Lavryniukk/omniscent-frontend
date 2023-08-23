4ee7becdb340f3da3650d46bd74fb430fb35f4f7
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-23 21:38:04 +0300

    testing gradient bg

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,13 +1,17 @@
 import TypingAnimation from "@/app/UI/TypingAnimation"
-// import HomeDescription from "@/app/UI/HomeDescription/HomeDescription"
 import { NextPage } from "next"
 import HomeButtons from "@/app/components/HomeButtons/HomeButtons"
+import PageGradient from "@/app/UI/PageGradient"
 
 let HomePage: NextPage = () => {
-    return <div className=" h-screen bg-no-repeat mx-auto box-border max-w-10xl  w-full pt-40 sm:pt-52 md:pt-64">
-            <h1 className="text-4xl text-text sm:text-5xl md:text-6xl lg:text-7xl font-roboto font-bold text-center ">Become <span className="text-primary">Omniscient</span></h1>
+    return <div className=" h-screen bg-transparent mx-auto box-border max-w-10xl
+                            w-full pt-40 sm:pt-52 md:pt-64">
+            <PageGradient/>                    
+            <h1 className="text-4xl tracking-wider text-text sm:text-5xl
+                            md:text-6xl lg:text-7xl font-roboto font-bold text-center "
+            >Become <span className="text-primary">Omniscient</span>
+            </h1>
             <TypingAnimation />
-            {/* <HomeDescription /> */}
             <HomeButtons />
         </div>
 }
--- app/components/HomeButtons/HomeButtons.tsx
+++ app/components/HomeButtons/HomeButtons.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 let HomeButtons = () => {
     return (
-        <div className="w-fit h-12 flex mx-auto mt-6 sm:mt-10 space-x-5 ">
+        <div className="w-fit h-12 flex mx-auto mt-10 space-x-5 ">
             <Link href={'/'} className="flex items-center justify-center bg-primary
                                         px-6  rounded-lg text-text font-roboto
                                         hover:bg-blue-700 transition-colors duration-300
