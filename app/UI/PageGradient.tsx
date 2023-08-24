0716dfdbec22f7ea10b7eff3f086d82d529369c1
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-24 13:16:45 +0300

    Bg images testing

--- app/UI/PageGradient.tsx
+++ app/UI/PageGradient.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image"
 let PageGradient = () => {
     return (
-        <div>
-        </div>
+        <>
+            <Image src={'/images/left-grad.png'} width={500} height={1000} className="opacity-1 absolute h-10lg w-1/2 top-14 left-0"  alt=""/>
+            <Image src={'/images/right-grad.png'} width={500} height={1000} className="opacity-1 absolute h-10lg w-1/2 top-14 right-0" alt=""/>
+        </>
     )
 }
 export default PageGradient
