098fb59d3bc984d6cc857782d1991bc19a101223
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-24 21:21:18 +0300

    Added navbar, made a beta-logo

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,18 +1,24 @@
-import TypingAnimation from "@/app/UI/TypingAnimation"
-import { NextPage } from "next"
-import HomeButtons from "@/app/components/HomeButtons/HomeButtons"
-import PageGradient from "@/app/UI/PageGradient"
+import TypingAnimation from "@/app/UI/TypingAnimation";
+import { NextPage } from "next";
+import HomeButtons from "@/app/components/HomeButtons/HomeButtons";
+import PageGradient from "@/app/UI/PageGradient";
 
 let HomePage: NextPage = () => {
-    return <div className=" overflow-hidden h-screen bg-transparent mx-auto box-border max-w-10xl
-                            w-full pt-40 sm:pt-52 md:pt-64">
-            <PageGradient/>                    
-            <h1 className="text-5xl tracking-wider text-text sm:text-6xl
+  return (
+    <div
+      className=" overflow-x-hidden h-screen bg-transparent mx-auto box-border max-w-10xl
+                            w-full pt-40 sm:pt-52 md:pt-64"
+    >
+      <PageGradient />
+      <h1
+        className="text-5xl tracking-wider text-text sm:text-6xl
                             md:text-7xl lg:text-8xl font-roboto font-bold text-center "
-            >Become <span className="text-primary">Omniscient</span>
-            </h1>
-            <TypingAnimation />
-            <HomeButtons />
-        </div>
-}
-export default HomePage
+      >
+        Become <span className="text-primary">Omniscient</span>
+      </h1>
+      <TypingAnimation />
+      <HomeButtons />
+    </div>
+  );
+};
+export default HomePage;
