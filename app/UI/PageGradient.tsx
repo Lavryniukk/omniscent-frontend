95aadbe12ac9271b334ae229a5dd357579aca332
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-24 13:17:53 +0300

    Minor change

--- app/UI/PageGradient.tsx
+++ app/UI/PageGradient.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 let PageGradient = () => {
     return (
         <>
-            <Image src={'/images/left-grad.png'} width={500} height={1000} className="opacity-1 absolute h-10lg w-1/2 top-14 left-0"  alt=""/>
-            <Image src={'/images/right-grad.png'} width={500} height={1000} className="opacity-1 absolute h-10lg w-1/2 top-14 right-0" alt=""/>
+            <Image src={'/images/left-grad.png'} width={500} height={1000} className="opacity-1 absolute h-10lg w-1/2 top-0 left-0"  alt=""/>
+            <Image src={'/images/right-grad.png'} width={500} height={1000} className="opacity-1 absolute h-10lg w-1/2 top-0 right-0" alt=""/>
         </>
     )
 }
