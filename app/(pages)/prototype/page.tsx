6b64cc506ed9da235f3f8423add2583f30b70844
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-28 21:58:14 +0300

    I pray not to broke anything

---
+++ app/(pages)/prototype/page.tsx
@@ -0,0 +1,11 @@
+import OrgChartTree from "@/app/modules/TreeMap/TreeMap";
+
+const ChartPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <OrgChartTree />
+    </div>
+  );
+};
+
+export default ChartPage;
