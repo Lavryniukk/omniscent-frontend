28b3dab80d78873d96cced280b134ae28a088a12
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-28 19:00:54 +0300

    Pricing page: tilted card

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -2,8 +2,8 @@ import TypingAnimation from "@/app/UI/TypingAnimation";
 import { NextPage } from "next";
 import HomeButtons from "@/app/components/HomeButtons/HomeButtons";
 import PageGradient from "@/app/UI/PageGradient";
-import Home from "@/app/modules/HomePage/Home";
-import Features from "@/app/modules/FeaturesPage/Features";
+import Home from "@/app/modules/homePage/Home";
+import Features from "@/app/modules/featuresPage/Features";
 
 let HomePage: NextPage = () => {
   return (
