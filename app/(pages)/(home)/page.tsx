84aeada0e2443971c232ca2401c0e43be713262a
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-28 20:24:19 +0300

    Starting working with d3 library, switching to the prototype page

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,7 +1,4 @@
-import TypingAnimation from "@/app/UI/TypingAnimation";
 import { NextPage } from "next";
-import HomeButtons from "@/app/components/HomeButtons/HomeButtons";
-import PageGradient from "@/app/UI/PageGradient";
 import Home from "@/app/modules/homePage/Home";
 import Features from "@/app/modules/featuresPage/Features";
 
