960fb74bf5d758ab5d8fa9a254805db4364f14fa
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-28 19:01:22 +0300

    bug fix

---
+++ app/(pages)/pricing/page.tsx
@@ -0,0 +1,10 @@
+import PricingCards from "@/app/modules/pricingPage/PricingCards";
+
+let Pricing = () => {
+  return (
+    <div>
+      <PricingCards />
+    </div>
+  );
+};
+export default Pricing;
