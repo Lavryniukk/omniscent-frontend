566b7694bfee28339ae4bd196f498119fd3c070f
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 21:08:39 +0300

    Bug fix

--- app/modules/pricingPage/PricingCards.tsx
+++ app/modules/pricingPage/PricingCards.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { FaCrown } from "react-icons/fa";
-import Tilt from "react-parallax-tilt";
 let PricingCards = () => {
   return (
     <div className=" max-w-10xl mt-40 mx-auto flex flex-row justify-around items-center">
@@ -12,12 +11,7 @@ let PricingCards = () => {
           Trial edition
         </h1>
       </div>
-      <Tilt
-        className="h-[600px] w-[400px] rounded-xl p-3.5 border-primary border-2 bg-secondary preserve-3d"
-        perspective={1000}
-        glareEnable={true}
-        glareMaxOpacity={0.15}
-      >
+      <div className="h-[600px] w-[400px] rounded-xl p-3.5 border-primary border-2 bg-secondary preserve-3d">
         <h3 className=" text-base  italic font-roboto text-start mx-1 text-accent bg-clip-text">
           * best choice
         </h3>
@@ -25,7 +19,7 @@ let PricingCards = () => {
         <h1 className=" text-4xl text-text font-semibold mt-2 ml-5">
           Learner edition
         </h1>
-      </Tilt>
+      </div>
       <div className="h-[500px] w-[300px] rounded-xl p-3.5 border-accent  opacity-60 border bg-secondary">
         <h3 className=" text-base  italic font-roboto text-start mx-1 text-accent bg-clip-text">
           *all-in-one
