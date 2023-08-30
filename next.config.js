1818d61e80a095e10b0aad4798aec91210536b6b
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-31 00:16:21 +0300

    vercel bug fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
+const nextConfig = {};
+module.exports = {
+  webpack5: true,
+  // Other configurations can be added here
+};
+module.exports = nextConfig;
