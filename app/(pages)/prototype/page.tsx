4703d72ebda36a2a09f8bc7f10c68ffb0b35a0a4
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-31 00:32:04 +0300

    bug fixing i guess?

--- app/(pages)/prototype/page.tsx
+++ app/(pages)/prototype/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 import dynamic from "next/dynamic";
-const DynamicComponentTree = dynamic(
-  () => import("@/app/modules/TreeMap/TreeMap"),
-  { ssr: false }
-);
+import GraphComponent from "@/app/modules/TreeMap/TreeMap";
+// const DynamicComponentTree = dynamic(
+//   () => import("@/app/modules/TreeMap/TreeMap"),
+//   { ssr: false }
+// );
 import React from "react";
 const PrototypePage = () => {
   return (
     <div className=" w-full mt-16">
-      <DynamicComponentTree />
+      <GraphComponent />
     </div>
   );
 };
--- next.config.js
+++ next.config.js
@@ -2,17 +2,5 @@
 const nextConfig = {};
 module.exports = nextConfig;
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.target = "electron-renderer";
-      config.node = {
-        __dirname: true,
-      };
-    }
-    return config;
-  },
-};
-module.exports = {
   webpack5: true,
-  // Other configurations can be added here
 };
