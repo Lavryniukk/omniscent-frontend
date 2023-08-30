deb89e068eee7f23f9d8bc5a426a2faa2130c942
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 21:46:08 +0300

    Bug fix(not)

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -3,10 +3,10 @@ import Home from "@/app/modules/homePage/Home";
 
 let HomePage = () => {
   return (
-    <>
+    <div>
       <Home />
       <Features />
-    </>
+    </div>
   );
 };
 export default HomePage;
