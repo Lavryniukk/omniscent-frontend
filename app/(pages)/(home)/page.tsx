6831e068a0805301523a78e66caad7bc6f708be5
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 15:46:42 +0300

    Minor bug fix

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,8 +1,7 @@
-import { NextPage } from "next";
 import Home from "@/app/modules/homePage/Home";
 import Features from "@/app/modules/featuresPage/Features";
 
-let HomePage: NextPage = () => {
+let HomePage = () => {
   return (
     <>
       <Home />
