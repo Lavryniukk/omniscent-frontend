30488919657c5f758e13b1a437260703bc9e30dd
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-31 00:29:10 +0300

    One more bug fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = nextConfig;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.target = "electron-renderer";
+      config.node = {
+        __dirname: true,
+      };
+    }
+    return config;
+  },
+};
 module.exports = {
   webpack5: true,
   // Other configurations can be added here
 };
-module.exports = nextConfig;
