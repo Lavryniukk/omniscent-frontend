967f0d2370465c58bdc94dc5d11507f692b48a03
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 21:52:34 +0300

    Fix bug ()typing

--- app/modules/HomePage/Home.tsx
+++ app/modules/HomePage/Home.tsx
@@ -1,5 +1,5 @@
 import PageGradient from "@/app/UI/PageGradient";
-// import TypingAnimation from "@/app/UI/TypingAnimation";
+import TypingAnimation from "@/app/UI/TypingAnimation";
 // import HomeButtons from "@/app/components/HomeButtons/HomeButtons";
 
 const Home = () => {
@@ -9,14 +9,14 @@ const Home = () => {
 					  w-full py-40 sm:py-52 md:py-64"
     >
       <PageGradient />
-      {/* <h1
+      <h1
         className="text-5xl tracking-wider text-text sm:text-6xl
 					  md:text-7xl lg:text-8xl font-roboto font-bold text-center "
       >
         Become <span className="text-primary">Omniscient</span>
       </h1>
       <TypingAnimation />
-      <HomeButtons /> */}
+      {/* <HomeButtons /> */}
     </div>
   );
 };
