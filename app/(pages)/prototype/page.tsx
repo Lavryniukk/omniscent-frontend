b59a6af18f619ea6481b369f5e3b42558fd3acbd
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 22:18:52 +0300

    Yes or not

--- app/(pages)/prototype/page.tsx
+++ app/(pages)/prototype/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
+import Home from "@/app/modules/homePage/Home";
 const DynamicComponentTree = dynamic(
   () => import("@/app/modules/TreeMap/TreeMap"),
   { ssr: false }
@@ -9,6 +10,7 @@ const PrototypePage = () => {
   return (
     <div className=" w-full mt-16">
       <DynamicComponentTree />
+      <Home />
     </div>
   );
 };
