c6200d3bb2ed21a55201a5fdb7f6e3ec2be4c76f
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 22:00:54 +0300

    Snos

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,11 +1,15 @@
-// import Features from "@/app/modules/featuresPage/Features";
-// import Home from "@/app/modules/homePage/Home";
-
+import dynamic from "next/dynamic";
+const Features = dynamic(() => import("@/app/modules/featuresPage/Features"), {
+  ssr: false,
+});
+const Home = dynamic(() => import("@/app/modules/homePage/Home"), {
+  ssr: false,
+});
 let HomePage = () => {
   return (
     <>
-      {/* <Home />
-      <Features /> */}
+      <Home />
+      <Features />
     </>
   );
 };
