016d1a298f04cfd68482061f11d1fd1df10126af
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 19:08:53 +0300

    Fixing bug

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,10 +1,10 @@
-import Home from "@/app/modules/homePage/Home";
+// import Home from "@/app/modules/homePage/Home";
 import Features from "@/app/modules/featuresPage/Features";
 
 let HomePage = () => {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
       <Features />
     </>
   );
