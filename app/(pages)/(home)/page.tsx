1b2a1c70ac7bc92b806256373588f7b60fe886b8
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 21:55:57 +0300

    last chance

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,11 +1,11 @@
-import Features from "@/app/modules/featuresPage/Features";
-import Home from "@/app/modules/homePage/Home";
+// import Features from "@/app/modules/featuresPage/Features";
+// import Home from "@/app/modules/homePage/Home";
 
 let HomePage = () => {
   return (
     <>
-      <Home />
-      <Features />
+      {/* <Home />
+      <Features /> */}
     </>
   );
 };
