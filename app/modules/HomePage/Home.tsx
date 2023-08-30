fb66a2ee44312c5342af3de4dac27f819b0d0fb6
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-30 21:51:20 +0300

    fix bug(

--- app/modules/HomePage/Home.tsx
+++ app/modules/HomePage/Home.tsx
@@ -1,6 +1,6 @@
-import PageGradient from "@/app/UI/PageGradient";
-import TypingAnimation from "@/app/UI/TypingAnimation";
-import HomeButtons from "@/app/components/HomeButtons/HomeButtons";
+// import PageGradient from "@/app/UI/PageGradient";
+// import TypingAnimation from "@/app/UI/TypingAnimation";
+// import HomeButtons from "@/app/components/HomeButtons/HomeButtons";
 
 const Home = () => {
   return (
@@ -8,7 +8,7 @@ const Home = () => {
       className="home overflow-x-hidden h-fit bg-transparent mx-auto box-border max-w-10xl
 					  w-full py-40 sm:py-52 md:py-64"
     >
-      <PageGradient />
+      {/* <PageGradient />
       <h1
         className="text-5xl tracking-wider text-text sm:text-6xl
 					  md:text-7xl lg:text-8xl font-roboto font-bold text-center "
@@ -16,7 +16,7 @@ const Home = () => {
         Become <span className="text-primary">Omniscient</span>
       </h1>
       <TypingAnimation />
-      <HomeButtons />
+      <HomeButtons /> */}
     </div>
   );
 };
