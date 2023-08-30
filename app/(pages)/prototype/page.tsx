3b2c195f8c57a753c70afbd09bfee8290211a06e
Lavryniuk Lev <leoyt109@gmail.com>
2023-08-31 00:39:36 +0300

    I think this will fix the Bug

--- app/(pages)/prototype/page.tsx
+++ app/(pages)/prototype/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import dynamic from "next/dynamic";
-import GraphComponent from "@/app/modules/TreeMap/TreeMap";
-// const DynamicComponentTree = dynamic(
-//   () => import("@/app/modules/TreeMap/TreeMap"),
-//   { ssr: false }
-// );
+const DynamicComponentTree = dynamic(
+  () => import("@/app/modules/TreeMap/TreeMap"),
+  { ssr: false }
+);
 import React from "react";
 const PrototypePage = () => {
   return (
     <div className=" w-full mt-16">
-      <GraphComponent />
+      <DynamicComponentTree />
     </div>
   );
 };
