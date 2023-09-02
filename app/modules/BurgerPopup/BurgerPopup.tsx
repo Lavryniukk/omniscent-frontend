09a0790f810585c766f5e18425cff59ffa384c6b
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-03 00:15:49 +0300

    Minor changse

--- app/modules/BurgerPopup/BurgerPopup.tsx
+++ app/modules/BurgerPopup/BurgerPopup.tsx
@@ -11,14 +11,14 @@ let Popup = ({ isOpen }: Props) => {
         isOpen ? "right-0" : "-right-36"
       } fixed h-full border-l w-3/12 transition-all py-10 backdrop-blur-md duration-200 flex flex-col space-y-5 justify-start items-center`}
     >
-      <div>
+      <div className="flex flex-col space-y-5 items-center">
         <HeaderLink url="" name="Home" />
         <HeaderLink url="" name="Prototype" />
         <HeaderLink url="" name="FAQ" />
         <HeaderLink url="" name="About us" />
       </div>
-      <div className="w-8 h-0.5 bg-accent " />
-      <div className="mt-5">
+      <div className="w-10 h-0.5 bg-accent " />
+      <div className="mt-5 flex flex-col space-y-5 items-center">
         <HeaderLink url="" name="Sign in" />
         <HeaderLink url="" name="Sign up" />
       </div>
