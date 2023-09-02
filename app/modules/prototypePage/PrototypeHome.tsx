8d502d9304c0501b520cd1d5e205bb0667309498
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-02 22:25:42 +0300

    Changed back button, Some minor changes on prototype page

--- app/modules/prototypePage/PrototypeHome.tsx
+++ app/modules/prototypePage/PrototypeHome.tsx
@@ -1,15 +1,13 @@
-import Input from "@/app/(pages)/prototype/components/PrototypeInput";
-import Title from "@/app/(pages)/prototype/components/PrototypeTItle";
+import Input from "@/app/components/prototypeComponents/PrototypeInput";
+import Title from "@/app//components/prototypeComponents/PrototypeTItle";
+import Boxes from "@/app/components/prototypeComponents/PrototypeBoxes";
 
 let PrototypeMain = () => {
   return (
     <div className=" w-fit mx-auto mt-60">
       <Title />
       <Input />
-      <div className="flex flex-row justify-around mt-10 select-none">
-        <div className="w-4/12 border-2 aspect-square rounded-2xl border-secondary hover:border-accent"></div>
-        <div className="w-4/12 border-2 aspect-square rounded-2xl border-secondary hover:border-accent"></div>
-      </div>
+      <Boxes />
     </div>
   );
 };
