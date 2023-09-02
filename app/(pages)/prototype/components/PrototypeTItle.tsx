d53171e63a2a74e8e46b666ae0e1e9d45e9960de
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-02 20:21:31 +0300

    Added some design on prototype page

--- app/(pages)/prototype/components/PrototypeTItle.tsx
+++ app/(pages)/prototype/components/PrototypeTItle.tsx
@@ -1,6 +1,6 @@
 let Title = () => {
   return (
-    <h1 className="text-text text-5xl font-roboto font-bold w-fit text-center mx-auto">
+    <h1 className="text-text text-zinc-300 font-roboto md:text-4xl sm:text-3xl text-2xl lg:text-5xl font-bold w-fit text-center mx-auto">
       You are on the prototype page
     </h1>
   );
