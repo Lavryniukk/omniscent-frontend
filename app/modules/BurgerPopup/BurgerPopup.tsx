73ef1b158e6a5bd6dff4c9df65c12a6cdb1b08df
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-03 00:12:16 +0300

    Minor header changse

--- app/modules/BurgerPopup/BurgerPopup.tsx
+++ app/modules/BurgerPopup/BurgerPopup.tsx
@@ -11,12 +11,17 @@ let Popup = ({ isOpen }: Props) => {
         isOpen ? "right-0" : "-right-36"
       } fixed h-full border-l w-3/12 transition-all py-10 backdrop-blur-md duration-200 flex flex-col space-y-5 justify-start items-center`}
     >
-      <HeaderLink url="" name="Home" />
-      <HeaderLink url="" name="Prototype" />
-      <HeaderLink url="" name="FAQ" />
-      <HeaderLink url="" name="About us" />
-      <HeaderLink url="" name="Sign in" />
-      <HeaderLink url="" name="Sign up" />
+      <div>
+        <HeaderLink url="" name="Home" />
+        <HeaderLink url="" name="Prototype" />
+        <HeaderLink url="" name="FAQ" />
+        <HeaderLink url="" name="About us" />
+      </div>
+      <div className="w-8 h-0.5 bg-accent " />
+      <div className="mt-5">
+        <HeaderLink url="" name="Sign in" />
+        <HeaderLink url="" name="Sign up" />
+      </div>
     </div>
   );
 };
