e0c93007f7c20af25f5d3e7e9ea16563dd1b0a88
Lev Lavryniuk <leoyt109@gmail.com>
2023-09-04 16:04:03 +0300

    Reworked sidebar system

--- app/modules/prototype/treeMap/TreeMap.tsx
+++ app/modules/prototype/treeMap/TreeMap.tsx
@@ -1,168 +1,21 @@
 "use client";
 import { treenode } from "@/app/shared/types/node";
-import { useState } from "react";
 import { Tree, TreeNode } from "react-organizational-chart";
 
-const roadmap: treenode = {
-  id: "become",
-  name: "become a programmer",
-  displayChildren: true,
-  children: [
-    {
-      id: "languages",
-      name: "Languages",
-      displayChildren: true,
-      children: [
-        {
-          id: "ts",
-          name: "TypeScript",
-          displayChildren: true,
-          children: [
-            {
-              id: "syntax",
-              name: "Syntax",
-              displayChildren: true,
-              children: [],
-            },
-            {
-              id: "frameworks",
-              name: "Frameworks",
-              displayChildren: true,
-              children: [
-                {
-                  id: "react",
-                  name: "React",
-                  displayChildren: true,
-                  children: [],
-                },
-                {
-                  id: "vue",
-                  name: "Vue",
-                  displayChildren: true,
-                  children: [],
-                },
-                {
-                  id: "angular",
-                  name: "Angular",
-                  displayChildren: true,
-                  children: [],
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: "js",
-          name: "JavaScript",
-          displayChildren: true,
-          children: [
-            {
-              id: "nodejs",
-              name: "NodeJs",
-              displayChildren: true,
-              children: [],
-            },
-            {
-              id: "syntax",
-              name: "Syntax",
-              displayChildren: true,
-              children: [
-                {
-                  id: "ifelse",
-                  name: "if/else",
-                  displayChildren: true,
-                  children: [],
-                },
-                {
-                  id: "let",
-                  name: "Let",
-                  displayChildren: true,
-                  children: [],
-                },
-              ],
-            },
-            {
-              id: "dom",
-              name: "DOM",
-              displayChildren: true,
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: "tools",
-      name: "Tools",
-      displayChildren: true,
-      children: [
-        {
-          id: "git",
-          name: "Git",
-          displayChildren: true,
-          children: [],
-        },
-        {
-          id: "vscode",
-          name: "VSCode",
-          displayChildren: true,
-          children: [],
-        },
-      ],
-    },
-    {
-      id: "concepts",
-      name: "Concepts",
-      displayChildren: true,
-      children: [
-        {
-          id: "http",
-          name: "HTTP",
-          displayChildren: true,
-          children: [],
-        },
-        {
-          id: "www",
-          name: "WWW",
-          displayChildren: true,
-          children: [],
-        },
-        {
-          id: "development",
-          name: "Development process",
-          displayChildren: true,
-          children: [],
-        },
-      ],
-    },
-  ],
-};
 type Props = {
   toggleSideBar: () => void;
   showSideBar: boolean;
+  tree: treenode;
+  setNewTree: (node: treenode) => void;
   selectNode: (node: treenode) => void;
 };
-const GraphComponent = ({ toggleSideBar, showSideBar, selectNode }: Props) => {
-  let [tree, setTree] = useState<treenode>(roadmap);
-  const toggleNode = (id: string, node: treenode): treenode => {
-    if (node.id === id && node.children) {
-      return { ...node, displayChildren: !node.displayChildren };
-    }
-
-    if (node.children) {
-      return {
-        ...node,
-        children: node.children.map((child) => toggleNode(id, child)),
-      };
-    }
-
-    return node;
-  };
-  const toggleChildren = (id: string) => {
-    const newTree = toggleNode(id, tree);
-    setTree(newTree);
-  };
-
+const GraphComponent = ({
+  toggleSideBar,
+  showSideBar,
+  setNewTree,
+  selectNode,
+  tree,
+}: Props) => {
   const generateTree = (treeNode: treenode) => {
     const { id, name, displayChildren, children } = treeNode;
     return (
@@ -175,8 +28,6 @@ const GraphComponent = ({ toggleSideBar, showSideBar, selectNode }: Props) => {
               className="node-circle"
               onClick={() => {
                 selectNode(treeNode);
-                toggleSideBar();
-                toggleChildren(id);
               }}
             />
             <div className="node">{name}</div>
--- app/shared/prototypeSideBar/Sbar.tsx
+++ app/shared/prototypeSideBar/Sbar.tsx
@@ -2,19 +2,39 @@ import { treenode } from "../types/node";
 
 type Props = {
   showSideBar: boolean;
+  tree: treenode;
+  toggleChildren: (id: string) => void;
   toggleSideBar: () => void;
-  selectedNode?: treenode;
+  selectedNode: treenode;
 };
-let SideBar = ({ showSideBar, toggleSideBar, selectedNode }: Props) => {
+let SideBar = ({
+  showSideBar,
+  toggleSideBar,
+  toggleChildren,
+  selectedNode,
+}: Props) => {
   let res = showSideBar ? "right-0" : "-right-1/4";
+  let hideBtnText = selectedNode.displayChildren
+    ? "Hide children"
+    : "Show children"
+    ? "Hide children"
+    : "Show children";
   return (
     <div
-      className={`border-l-2 fixed top-0 transition-all duration-300  ${res} text-text h-screen w-1/4 `}
+      className={`border-l-2 fixed top-0 transition-all duration-300 ${res} text-text h-screen w-1/4 `}
     >
-      <div className="border h-20 w-8 "></div>
-      <div className="w-fit flex items-center justify-center px-4 py-2 hover:border-text rounded-lg m-2 border border-accent h-fit">
-        {/* {selectedNode?.displayChildren ? 'Hide children' ? 'Show children'} */}
+      <div className="flex flex-row m-2">
+        <div className="border w-20 h-8  top-1/2 left-0"></div>
+        <div
+          onClick={() => {
+            toggleChildren(selectedNode.id);
+          }}
+          className="w-fit flex items-center justify-center px-4 py-2 hover:border-text rounded-lg  border border-accent h-fit"
+        >
+          {hideBtnText}
+        </div>
       </div>
+      <h1 className=" text-2xl text-center">{selectedNode.name}</h1>
     </div>
   );
 };
