622bd26af34f6e115de5488ed3dbc89083d74ec6
Lev Lavryniuk <leoyt109@gmail.com>
2023-09-13 20:25:54 +0300

    Fixed minor bugs with tree moving

--- app/(pages)/prototype/layout.tsx
+++ app/(pages)/prototype/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${roboto.variable}`}>
+    <html lang="en" className={`${roboto.variable} overflow-hidden`}>
       <body>
         <HomeButton />
         {children}
