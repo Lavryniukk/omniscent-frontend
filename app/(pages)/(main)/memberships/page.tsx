7e89cce97d9bb6da88540557185064eba6f95183
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-16 17:49:23 +0300

    Moved pricing to new page

---
+++ app/(pages)/(main)/memberships/page.tsx
@@ -0,0 +1,10 @@
+import Pricing from "@/app/modules/memberships/PricingSection/Pricing";
+
+let MembershipsPage = () => {
+  return (
+    <>
+      <Pricing />
+    </>
+  );
+};
+export default MembershipsPage;
