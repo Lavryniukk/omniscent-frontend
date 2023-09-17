a607612758ac61ddb69e948245068c8422c7d982
Ivan Larin <n.larivanko@gmail.com>
2023-09-17 15:59:57 +0300

    added responsoviness for pricing heading and minor bug fixes

--- app/modules/memberships/FAQSection/PricingFAQ.tsx
+++ app/modules/memberships/FAQSection/PricingFAQ.tsx
@@ -1,4 +1,4 @@
-import PricingFAQItem from "@/app/components/pricingFAQComponents/PricingFAQItem";
+import FAQItem from "@/app/components/FAQComponents/FAQItem";
 
 let PricingFAQ = () => {
   return (
@@ -6,32 +6,32 @@ let PricingFAQ = () => {
       className="space-y-10 select-none pricing overflow-x-hidden px-10 bg-transparent mx-auto box-border max-w-10xl
   w-full my-60 h-fit justify-around items-center"
     >
-      <h2 className="text-5xl  font-roboto mx-auto font-bold text-center text-text">
+      <h2 className="text-4xl sm:text-5xl  font-roboto mx-auto font-bold text-center text-text">
         FAQ
       </h2>
 
       <div className="sm:p-8 p-4 space-y-5">
-        <PricingFAQItem
+        <FAQItem
           question="How much does your product/service cost?"
           answer="Our pricing varies depending on the plan you choose. Please visit our pricing page for detailed information."
         />
 
-        <PricingFAQItem
+        <FAQItem
           question="Do you offer a free trial?"
           answer="Yes, we provide a free trial for 14 days so you can explore our features before committing."
         />
 
-        <PricingFAQItem
+        <FAQItem
           question="Is there a discount for annual subscriptions?"
           answer="Absolutely! We offer significant discounts for annual subscriptions compared to monthly plans."
         />
 
-        <PricingFAQItem
+        <FAQItem
           question="Can I change my plan later?"
           answer="Yes, you can upgrade or downgrade your plan at any time to suit your changing needs."
         />
 
-        <PricingFAQItem
+        <FAQItem
           question="What payment methods do you accept?"
           answer="We accept major credit cards, PayPal, and other secure payment options for your convenience."
         />
--- app/modules/memberships/PricingSection/Pricing.tsx
+++ app/modules/memberships/PricingSection/Pricing.tsx
@@ -2,12 +2,12 @@ let Pricing = () => {
   return (
     <div className="w-full border-secondary from-slate-950 to-background">
       <div className="gradient h-[1000px] hidden 2xl:block center-align top-[230px] -z-10 blur-[160px] opacity-70 w-[1000px] absolute" />
-      <div className="box-border items-end justify-around w-full px-10 mx-auto my-32 overflow-x-hidden bg-transparent border select-none max-w-10xl h-fit">
-        <h2 className="mx-auto my-20 font-bold text-center text-text text-7xl font-roboto">
+      <div className="box-border items-end justify-around w-full lg:px-10 mx-auto my-32 overflow-x-hidden bg-transparent select-none max-w-10xl h-fit">
+        <h2 className="mx-auto my-20 font-bold text-center text-text text-4xl sm:text-5xl md:text-7xl font-roboto">
           Choose a plan that
           <br /> fits your style
         </h2>
-        <div className=" w-full flex 2xl:flex-nowrap flex-wrap flex-col border gap-5 max-w-[1400px] 2xl:h-[500px] lg:h-[1000px] h-[1600px] lg:flex-row relative 2xl:items-end mx-auto">
+        <div className="w-full flex 2xl:flex-nowrap flex-wrap flex-col gap-5 max-w-[1400px] 2xl:h-[500px] lg:h-[1000px] h-[1600px] lg:flex-row relative 2xl:items-end mx-auto">
           <div className="2xl:w-1/4 mx-auto lg:w-4/12 w-[350px] p-8 border-2 bg-background 2xl:h-[80%] lg:h-[45%] h-[30%] bg-opacity-90 rounded-xl border-secondary">
             <h1 className="mb-2 text-2xl font-light text-left text-text">
               Trial
