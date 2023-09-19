41d1f292a3e83c1e93f4b0deb458d24b47a12c01
Ivan Larin <n.larivanko@gmail.com>
2023-09-19 14:06:51 +0300

    minor bug fix

--- app/components/FAQComponents/FAQItem.tsx
+++ app/components/FAQComponents/FAQItem.tsx
@@ -14,7 +14,7 @@ let FAQItem = ({ question, answer }: Props) => {
   return (
     <div
       className={`border-x-[1.5px] border-accent lg:w-4/6 sm:w-5/6  text-text mx-auto ${
-        !isOpen ? "h-12" : "sm:h-32 h-36 lg:h-28"
+        !isOpen ? "h-12" : "sm:h-32 h-36"
       } sm:px-2 px-1 transition-all duration-500 overflow-hidden lg:text-lg sm:text-base text-[13px]`}
       onClick={handleClick}
     >
