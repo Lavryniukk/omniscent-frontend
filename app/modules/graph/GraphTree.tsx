93d1b0c6ce62bea0572326e86ea3695cf34b6cd0
Lev Lavryniuk <leoyt109@gmail.com>
2023-09-20 14:19:28 +0300

    save commit

--- app/modules/graph/GraphTree.tsx
+++ app/modules/graph/GraphTree.tsx
@@ -1,39 +1,56 @@
 "use client";
 import { treenode } from "@/app/shared/types/node";
-let getAfter = (node: treenode, nodeArray: Array<treenode>) => {
-  let lastEl: treenode = nodeArray[-1];
+let getAfter = (node: treenode, nodeArray: Array<treenode>, height: number) => {
+  let firstEl: treenode = nodeArray[0];
+  let rounded = "";
+  let lastEl: treenode = nodeArray[nodeArray.length - 1];
   let after: string = "";
+  if (node == firstEl) {
+    rounded = " after:rounded-bl-2xl after:border-l-2";
+  }
   if (lastEl !== node) {
-    after =
-      "after:absolute after:right-1/2 after:bg-background after:w-1/2 after:bottom-0 after:h-10 after:border";
+    after = `after:absolute after:right-0 ${rounded} after:h-${height.toString} after:border-b-2 after:border-accent after:w-1/2 after:bottom-0 `;
   }
 
   return after;
 };
+let getBefore = (
+  node: treenode,
+  nodeArray: Array<treenode>,
+  height: number
+) => {
+  let firstEl: treenode = nodeArray[0];
+  let before: string = "";
+  let rounded = "";
+  let lastEl: treenode = nodeArray[nodeArray.length - 1];
+  if (node == lastEl) {
+    rounded = " before:rounded-br-2xl";
+  }
+  if (firstEl !== node) {
+    before = `before:absolute before:left-0 ${rounded} before:h-${height.toString()} before:border-b-2 before:border-accent before:w-1/2 before:bottom-0 before:border-r-2`;
+  }
+
+  return before;
+};
 let GraphTree = ({ treeObjectArray }: { treeObjectArray: Array<treenode> }) => {
+  let heightOfSpacing = 10;
   let generateTreeGraph = () => {
     let result = treeObjectArray.map((node) => {
-      let after = getAfter(node, treeObjectArray);
-      let before = getBefore(node, treeObjectArray);
-      console.log(afterBefore);
+      let after = getAfter(node, treeObjectArray, heightOfSpacing);
+      let before = getBefore(node, treeObjectArray, heightOfSpacing);
       return (
-        <div
-          className={`w-fit flex relative flex-col-reverse h-60 border ${
-            (afterBefore[1], afterBefore[0])
-          }`}
+        <ul
+          key={node.id}
+          className={` w-28 ${after} ${before}  relative text-center text-accent`}
         >
-          <ul key={node.id} className="border w-40 h-40 text-accent">
+          <div key={node.id} className="border h-20">
             {node.name}
-          </ul>
-        </div>
+          </div>
+        </ul>
       );
     });
     return result;
   };
-  return (
-    <div className="w-fit h-fit border flex flex-row">
-      {generateTreeGraph()}
-    </div>
-  );
+  return <div className="w-fit h-fit flex flex-row">{generateTreeGraph()}</div>;
 };
 export default GraphTree;
