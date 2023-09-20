0fbf8ebe7760d6e68a4f6b30722146c2a7d7d83d
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-20 22:44:33 +0300

    Beta version of lib (do not touch under any circumstanses)

--- app/modules/graph/GraphTree.tsx
+++ app/modules/graph/GraphTree.tsx
@@ -5,10 +5,12 @@ let getAfter = (node: treenode, nodeArray: Array<treenode>, height: string) => {
   let rounded = "";
   let after: string = "";
   if (node == firstEl) {
-    rounded = " after:rounded-bl-2xl";
+    rounded = " after:rounded-bl-2xl  after:border-l-2";
+  } else {
+    rounded = "after:border-l";
   }
   if (lastEl !== node) {
-    after = `after:absolute after:right-0 ${rounded} after:border-l-2 after:h-10 after:border-b-2 after:border-accent after:w-1/2 after:bottom-0 `;
+    after = `after:absolute after:right-0 ${rounded} after:h-[${height}px] after:border-b-2 after:border-accent after:w-1/2 after:bottom-0 `;
   }
 
   return after;
@@ -24,30 +26,35 @@ let getBefore = (
   let before: string = "";
   if (node == lastEl) {
     rounded = " before:rounded-br-2xl before:border-r-2";
+  } else {
+    rounded = "before:border-r";
   }
   if (firstEl !== node) {
-    before = `before:absolute before:left-0 ${rounded} before:border-b-2 before:h-10 before:border-accent before:w-1/2 before:bottom-0 `;
+    before = `before:absolute before:left-0 ${rounded}  before:h-[${height}px] before:border-b-2  before:border-accent before:w-1/2 before:bottom-0 `;
   }
 
   return before;
 };
 let GraphTree = ({ treeObjectArray }: { treeObjectArray: Array<treenode> }) => {
-  let heightOfSpacing = "12";
+  let heightOfSpacing = "20";
   let generateTreeGraph = (tree: Array<treenode>) => {
     let result = tree.map((node) => {
       let arrowTopAfter =
-        "after:absolute after:right-1/2 after:-top-4  after:bg-accent after:h-5 after:w-0.5";
+        "after:absolute after:right-1/2 after:-top-5  after:bg-accent after:h-6 after:w-0.5";
       let after = getAfter(node, tree, heightOfSpacing);
       let before = getBefore(node, tree, heightOfSpacing);
       return node.children ? (
         <ul
           key={node.id}
-          className={`w-fit  ${after} ${before} p-1 flex-col-reverse pb-10 flex relative text-center text-accent`}
+          className={`w-fit ${after} ${before} p-3 flex-col-reverse pb-5 flex relative text-center text-accent`}
         >
-          <div
-            className={` w-full mx-auto mt-5 relative text-center ${arrowTopAfter}`}
-          >
+          <div className={` w-full mx-auto pt-2 mt-5 relative text-center `}>
             {node.name}
+            <div
+              className={`w-10 aspect-square mt-2 rounded-full ${
+                node.children.length ? arrowTopAfter : ""
+              } border-2 mx-auto border-accent`}
+            />
           </div>
           <div className="flex flex-row h-fit w-fit">
             {generateTreeGraph(node.children)}
