f4708bf27adf45a77df5df2bfbd6e72edf9c76a1
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-21 13:48:14 +0300

    Finished library, new version of prototype roadmap

--- app/shared/prototypeSideBar/Sbar.tsx
+++ app/shared/prototypeSideBar/Sbar.tsx
@@ -3,18 +3,12 @@ import { MdOutlineClose } from "react-icons/md";
 // Define the Props type for the SideBar component
 type Props = {
   showSideBar: boolean; // Boolean prop to show/hide the sidebar
-  toggleChildren: () => void; // Function to toggle the display of children for a node
-  closeSideBar: () => void; // Function to close the sidebar
+  closeSideBar: (v: boolean) => void; // Function to close the sidebar
   selectedNode: treenode; // The currently selected node in the tree
 };
 
 // Define the SideBar functional component
-let SideBar = ({
-  showSideBar,
-  closeSideBar,
-  toggleChildren,
-  selectedNode,
-}: Props) => {
+let SideBar = ({ showSideBar, closeSideBar, selectedNode }: Props) => {
   // Calculate the CSS class for the sidebar position
   let res = showSideBar ? "right-0" : "-right-full";
 
@@ -33,21 +27,9 @@ let SideBar = ({
         {/* Close button */}
         <div
           className=" w-fit ml-auto m-1 cursor-pointer"
-          onClick={closeSideBar}
+          onClick={() => closeSideBar(false)}
         >
           <MdOutlineClose size={"30px"} color={"white"} /> {/* Close icon */}
-        </div>
-        {/* Toggle children button */}
-        <div className="flex flex-row justify-between w-full px-2 my-4">
-          <div
-            onClick={() => {
-              toggleChildren();
-            }}
-            className="w-fit flex items-center justify-center px-4 py-2 hover:border-text rounded-lg  border border-accent h-fit"
-          >
-            {selectedNode.displayChildren ? "Hide children" : "Show children"}
-            {/* Button label */}
-          </div>
         </div>
         {/* Node name */}
         <h1 className="text-2xl text-center">{selectedNode.name}</h1>
