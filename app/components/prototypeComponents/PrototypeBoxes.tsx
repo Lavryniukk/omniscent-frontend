f8c3becbc8e0893bde521bff687c50e997831fab
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-22 19:04:21 +0300

    Small changes in prototype page

--- app/components/prototypeComponents/PrototypeBoxes.tsx
+++ app/components/prototypeComponents/PrototypeBoxes.tsx
@@ -7,21 +7,21 @@ let Boxes = () => {
     <div className=" flex justify-center space-x-20 md:space-x-40 py-5 mt-10 select-none">
       <Link
         href={"/prototype/chat"}
-        className="w-1/4  max-w-[250px] max-h-[250px] pt-box-shadow flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
+        className="w-1/4  max-w-[200px] max-h-[200px] pt-box-shadow flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
       >
-        <p className="text-md sm:text-xl md:text-2xl  lg:text-3xl text-accent font-roboto font-light text-center mt-2 ">
-          Your chats
+        <p className="text-lg sm:text-xl md:text-2xl  lg:text-3xl text-accent font-roboto font-light text-center mt-2 ">
+          Chat
         </p>
-        <HiOutlineChatAlt2 className="text-accent text-5xl md:text-6xl lg:text-7xl  mx-auto" />
+        <HiOutlineChatAlt2 className="text-accent text-4xl md:text-5xl lg:text-6xl mx-auto" />
       </Link>
       <Link
         href={"/prototype/roadmap"}
-        className="w-1/4  max-w-[250px] max-h-[250px] pt-box-shadow flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
+        className=" w-1/4  max-w-[200px] max-h-[200px] pt-box-shadow flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
       >
-        <p className="text-md sm:text-xl md:text-2xl  lg:text-3xl text-accent font-roboto font-light text-center mt-2 ">
-          Your roadmaps
+        <p className="text-lg sm:text-xl md:text-2xl  lg:text-3xl text-accent font-roboto font-light text-center mt-2 ">
+          Roadmap
         </p>
-        <ImTree className="text-accent text-5xl md:text-6xl lg:text-7xl  rotate-180 mx-auto" />
+        <ImTree className="text-accent text-4xl md:text-5xl lg:text-6xl rotate-180 mx-auto" />
       </Link>
     </div>
   );
