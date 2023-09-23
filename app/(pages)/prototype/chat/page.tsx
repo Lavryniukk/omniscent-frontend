eaa81a2f1a98d3904e9f466c2f2b44cb59908013
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-23 23:41:29 +0300

    save commit

--- app/(pages)/prototype/chat/page.tsx
+++ app/(pages)/prototype/chat/page.tsx
@@ -1,61 +1,98 @@
 "use client";
-// ads
-type Chat = { answer: string; question: string };
-import { useState, Fragment } from "react";
+type message = {
+  role: "user" | "assistant" | "system";
+  content: string;
+};
+import { useState } from "react";
+
 const Chat = () => {
-  const [question, setQuestion] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
+  const [chat, setChat] = useState<Array<message>>([
+    { role: "system", content: "Speak italiano" },
+    { role: "user", content: "Speak italiano" },
+    { role: "assistant", content: "Ok, bozo" },
+  ]);
 
-  const [chat, setChat] = useState<Array<Chat>>([]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuestion(e.target.value);
+  const addUserMessage = (content: string) => {
+    setChat([...chat, { role: "user", content: content }]);
+    console.log("Added user message: ", content);
   };
+  const addAssistantMessage = (assData: string) => {
+    setChat([...chat, { role: "assistant", content: assData }]);
+    console.log("Added ass message: ", assData);
+  };
+  const sendChatData = async () => {
+    let question = inputValue;
+    addUserMessage(question);
 
-  const handleSubmit = () => {
-    if (question.length > 0) {
-      const newItem = question;
+    let res = await fetch("https://model-prototype.onrender.com/model", {
+      method: "POST",
+      body: JSON.stringify({ messages: chat }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-      console.log("work");
+    if (res.body) {
+      let data: string = "";
 
-      setChat([...chat, { answer: sendData(newItem), question: newItem }]);
+      const reader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+          addAssistantMessage(data);
+
+          break;
+        }
+        data += value;
+        console.log(data);
+        setInputValue(data);
+      }
     }
   };
 
-  const sendData = (data: string) => {
-    console.log(data);
-
-    const answer = "answer";
-
-    return answer;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
   };
+  let result = chat.map((message: message, index: number) => {
+    switch (message.role) {
+      case "user":
+        return (
+          <div
+            key={index}
+            className="w-3/4 border rounded-lg text-text py-4 pl-8 flex items-center justify-start"
+          >
+            User: {message.content}
+          </div>
+        );
+      case "assistant":
+        return (
+          <div
+            key={index}
+            className="w-3/4 border rounded-lg text-accent py-4 pl-8 flex items-center justify-start"
+          >
+            Assistant: {message.content}
+          </div>
+        );
+    }
+  });
   return (
     <div
       className="space-y-10 select-none overflow-x-hidden px-10 bg-transparent mx-auto box-border max-w-10xl
 w-full my-32 h-fit"
     >
-      <div className="text-accent h-[60vh] w-full">
-        <div className=" rounded-lg space-y-4">
-          {chat.map((item: Chat, index: number) => {
-            return (
-              <Fragment key={index}>
-                <div className="bg-secondary">{item.question}</div>
-                <div className="bg-secondary">{item.answer}</div>
-              </Fragment>
-            );
-          })}
-        </div>
+      <div className="text-accent border flex-col w-full">
+        <div className=" rounded-lg space-y-4 border"> {result}</div>
       </div>
       <div className="w-10/12 h-10 select-none flex items-center mt-10 overflow-hidden border border-accent outline-none rounded-lg mx-auto ">
         <input
           onChange={(e) => {
             handleChange(e);
           }}
-          value={question}
+          value={inputValue}
           placeholder="I want to learn..."
           className="bg-transparent w-10/12 h-full px-2 text-accent box-border outline-none focus:border-text"
         />
         <button
-          onClick={handleSubmit}
+          onClick={sendChatData}
           type="submit"
           className="text-text bg-secondary duration-200 border border-accent opacity-70 hover:opacity-100 transition-all w-1/12 h-2/3 rounded-lg box-border"
         >
