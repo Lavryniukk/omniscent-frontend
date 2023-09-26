eb50e29a6c32ed69c8126592a8e4be70b1aff545
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-26 18:15:33 +0300

    minor changes

--- app/components/prototypeComponents/PrototypeBoxes.tsx
+++ app/components/prototypeComponents/PrototypeBoxes.tsx
@@ -6,7 +6,7 @@ let Boxes = () => {
   return (
     <div className=" flex justify-center space-x-20 md:space-x-40 py-5 mt-10 select-none">
       <div className="border-[0.5px] border-secondary w-1/4  flex relative  items-center group justify-center max-w-[200px] rounded-[20px] max-h-[200px]">
-        <div className="w-[40%] h-[40%] group-hover:w-[120%] blur-3xl group-hover:h-[120%] rounded-full border overflow-hidden  group-hover:spinner opacity-75 transition-all transi duration-[1s] z-[15]  bg-gradient-to-t from-sky-800 via-indigo-900 to-rose-700  absolute rotate-45"></div>
+        <div className="w-[40%] h-[40%] group-hover:w-[120%] blur-3xl group-hover:h-[120%] rounded-full border overflow-hidden  group-hover:spinner opacity-75 transition-all transi duration-[1s] z-[15]  bg-gradient-to-t from-primary via-indigo-900 to-rose-700  absolute rotate-45"></div>
 
         <Link
           href={"/prototype/chat"}
@@ -19,7 +19,7 @@ let Boxes = () => {
         </Link>
       </div>
       <div className="border-[0.5px] border-secondary w-1/4  flex relative items-center group justify-center max-w-[200px] rounded-[20px] max-h-[200px]">
-        <div className="w-[40%] h-[40%] group-hover:w-[120%] blur-3xl group-hover:h-[120%] rounded-full border overflow-hidden  group-hover:spinner opacity-75 transition-all transi duration-[1s] z-[15]  bg-gradient-to-t from-sky-800 via-indigo-900 to-rose-700  absolute rotate-45"></div>
+        <div className="w-[40%] h-[40%] group-hover:w-[120%] blur-3xl group-hover:h-[120%] rounded-full border overflow-hidden  group-hover:spinner opacity-75 transition-all transi duration-[1s] z-[15]  bg-gradient-to-t from-primary via-indigo-900 to-rose-700  absolute rotate-45"></div>
         <Link
           href={"/prototype/roadmap"}
           className=" w-[97%] h-[97%] g relative  z-20 bg-background  max-w-[200px] border-[0.5px] border-secondary max-h-[200px] flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
