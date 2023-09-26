c14fae3ecb982710cfbfa6964c181f92eee1de35
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-26 13:41:50 +0300

    Added super fancy hover on animation for button in prototype

--- app/components/prototypeComponents/PrototypeBoxes.tsx
+++ app/components/prototypeComponents/PrototypeBoxes.tsx
@@ -5,29 +5,29 @@ import { ImTree } from "react-icons/im";
 let Boxes = () => {
   return (
     <div className=" flex justify-center space-x-20 md:space-x-40 py-5 mt-10 select-none">
-      <div className="border-[0.5px] border-secondary w-1/4  flex relative overflow-hidden items-center group justify-center max-w-[200px] rounded-[20px] max-h-[200px]">
-        <div className="w-[50%] group-hover:w-[200%] overflow-hidden group-hover:rotate-[110] group-hover:spinner opacity-75  transition-all transi duration-[2s] z-[15] h-[50%] bg-gradient-to-t from-sky-800 via-indigo-900 to-rose-700 group-hover:h-[200%] absolute rotate-45" />
+      <div className="border-[0.5px] border-secondary w-1/4  flex relative  items-center group justify-center max-w-[200px] rounded-[20px] max-h-[200px]">
+        <div className="w-[40%] h-[40%] group-hover:w-[120%] blur-3xl group-hover:h-[120%] rounded-full border overflow-hidden  group-hover:spinner opacity-75 transition-all transi duration-[1s] z-[15]  bg-gradient-to-t from-sky-800 via-indigo-900 to-rose-700  absolute rotate-45"></div>
 
         <Link
           href={"/prototype/chat"}
           className=" w-[97%] h-[97%] relative  z-20 bg-background  max-w-[200px] border-[0.5px] border-secondary max-h-[200px] flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
         >
-          <p className="text-lg sm:text-xl md:text-2xl  lg:text-3xl text-accent font-roboto font-light text-center mt-2 ">
+          <p className="text-lg sm:text-xl group-hover:text-text md:text-2xl text-accent transition-colors duration-500 group-hover:opacity-80 lg:text-3xl  font-roboto font-bold text-center mt-2 ">
             Chat
           </p>
-          <HiOutlineChatAlt2 className="text-accent text-4xl md:text-5xl lg:text-6xl mx-auto" />
+          <HiOutlineChatAlt2 className="text-accent group-hover:opacity-80 group-hover:text-text  transition-colors duration-500 text-4xl md:text-5xl lg:text-6xl mx-auto" />
         </Link>
       </div>
-      <div className="border-[0.5px] border-secondary w-1/4  flex relative overflow-hidden items-center group justify-center max-w-[200px] rounded-[20px] max-h-[200px]">
-        <div className="w-[50%] group-hover:w-[200%] overflow-hidden group-hover:rotate-[110]  group-hover:spinner opacity-75 transition-all transi duration-[2s] z-[15] h-[50%] bg-gradient-to-t from-sky-800 via-indigo-900 to-rose-700 group-hover:h-[200%] absolute rotate-45" />
+      <div className="border-[0.5px] border-secondary w-1/4  flex relative items-center group justify-center max-w-[200px] rounded-[20px] max-h-[200px]">
+        <div className="w-[40%] h-[40%] group-hover:w-[120%] blur-3xl group-hover:h-[120%] rounded-full border overflow-hidden  group-hover:spinner opacity-75 transition-all transi duration-[1s] z-[15]  bg-gradient-to-t from-sky-800 via-indigo-900 to-rose-700  absolute rotate-45"></div>
         <Link
           href={"/prototype/roadmap"}
-          className=" w-[97%] h-[97%] relative  z-20 bg-background  max-w-[200px] border-[0.5px] border-secondary max-h-[200px] flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
+          className=" w-[97%] h-[97%] g relative  z-20 bg-background  max-w-[200px] border-[0.5px] border-secondary max-h-[200px] flex flex-col items-center justify-center space-y-5 transition-all duration-300 aspect-square rounded-2xl "
         >
-          <p className="text-lg sm:text-xl md:text-2xl  lg:text-3xl text-accent font-roboto font-light text-center mt-2 ">
+          <p className="text-lg sm:text-xl group-hover:text-text md:text-2xl text-accent transition-colors duration-500 group-hover:opacity-80 lg:text-3xl  font-roboto font-bold text-center mt-2 ">
             Roadmap
           </p>
-          <ImTree className="text-accent text-4xl md:text-5xl lg:text-6xl rotate-180 mx-auto" />
+          <ImTree className="text-accent group-hover:opacity-80 group-hover:text-text  transition-colors duration-500 text-4xl md:text-5xl lg:text-6xl rotate-180 mx-auto" />
         </Link>
       </div>
     </div>
