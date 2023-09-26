c2c135b45ae6c1ce2879af320177d9c4c3953a95
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-26 11:24:16 +0300

    Changed input in prototype

--- app/(pages)/prototype/page.tsx
+++ app/(pages)/prototype/page.tsx
@@ -1,4 +1,5 @@
 import PageGradient from "@/app/UI/PrototypePageGradient";
+import Grid from "@/app/components/prototypeComponents/PrototypeGrid";
 import PrototypeMain from "@/app/modules/prototype/PrototypeHome";
 import Image from "next/image";
 let PrototypePage = () => {
@@ -8,17 +9,7 @@ let PrototypePage = () => {
         left="/images/prototypebg-left.png"
         right="/images/prototypebg-right.png"
       />
-      <div>
-        <Image
-          className="fixed h-screen w-screen top-0 opacity-10 -z-10"
-          src={"/images/bg-image.jpg"}
-          width={1200}
-          height={1200}
-          alt=""
-          role="presentation"
-        />
-        <div className="w-screen h-screen -z-10 fixed top-0 bg-gradient-to-t from-background via-transparent to-background" />
-      </div>
+      <Grid />
       <PrototypeMain />
     </div>
   );
