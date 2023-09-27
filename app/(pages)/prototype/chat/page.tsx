ff1aacac4245a709de46f5e7c90c739517af89ba
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-27 17:48:21 +0300

    minor change

--- app/(pages)/prototype/chat/page.tsx
+++ app/(pages)/prototype/chat/page.tsx
@@ -65,7 +65,7 @@ const Chat = () => {
         return (
           <div
             key={index}
-            className="w-`full  border-accent rounded-lg text-accent py-4 lg:pl-8 pl-3 "
+            className="w-full border-accent h-fit rounded-lg text-accent py-4 lg:pl-8 pl-3 "
           >
             User: {message.content}
           </div>
@@ -74,7 +74,7 @@ const Chat = () => {
         return (
           <div
             key={index}
-            className="w-full rounded-lg  border-accent text-accent py-4 lg:pl-8 pl-3  bg-secondary"
+            className="w-full rounded-lg h-fit border-accent text-accent py-4 lg:pl-8 pl-3 bg-secondary"
           >
             Assistant:{" "}
             {chatRef.current[chatRef.current.length - 1] === message
@@ -90,10 +90,10 @@ const Chat = () => {
   };
   return (
     <div
-      className="space-y-10 select-none overflow-x-hidden  overflow-y-scroll px-10 bg-transparent mx-auto box-border max-w-10xl
+      className="space-y-10 select-none overflow-x-hidden overflow-y-scroll px-10 bg-transparent mx-auto box-border max-w-10xl
 w-full my-32 h-fit"
     >
-      <div className="text-accent flex-col w-3/4 overflow-y-scroll mx-auto">
+      <div className="text-accent flex-col bg-transparent w-3/4   mx-auto">
         {result}
       </div>
       <div className="w-10/12 h-10 select-none border backdrop-blur-md flex items-center overflow-hidden  border-accent outline-none rounded-lg absolute bottom-[10%] right-[8.3%]">
@@ -108,7 +108,7 @@ w-full my-32 h-fit"
           />
           <button
             type="submit"
-            className="text-text bg-secondary  duration-200 border border-accent opacity-70 hover:opacity-100 transition-all w-[100px] xl:ml-6 lg:ml-4 md:ml-4 sm:text-sm h-2/3 rounded-lg box-border"
+            className="text-text bg-secondary duration-200 border border-accent opacity-70 hover:opacity-100 transition-all w-[100px] xl:ml-6 lg:ml-4 md:ml-4 sm:text-sm h-2/3 rounded-lg box-border"
           >
             Send
           </button>
