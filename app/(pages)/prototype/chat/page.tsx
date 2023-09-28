8a65f0d671b33c77dac6c0db1dd92be1ace83568
Lavryniuk Lev <leoyt109@gmail.com>
2023-09-28 23:54:12 +0300

    save commit before layout change

--- app/(pages)/prototype/chat/page.tsx
+++ app/(pages)/prototype/chat/page.tsx
@@ -1,69 +1,15 @@
 "use client";
-import { message } from "@/app/shared/types/message";
-import useChatStore from "@/app/shared/storages/chatStorage";
-const Chat = () => {
-  const setInputData = useChatStore((state) => state.setInputData);
-  const assistantData = useChatStore((state) => state.assistantData);
-  const userInputData = useChatStore((state) => state.userInputData);
-  const isLoading = useChatStore((state) => state.isLoading);
-  const chat = useChatStore((state) => state.chat);
-  const sendData = useChatStore((state) => state.sendData);
 
-  let result = chat.map((message: message, index: number) => {
-    switch (message.role) {
-      case "user":
-        return (
-          <div
-            key={index}
-            className="w-full border-accent h-fit rounded-lg text-accent py-4 lg:pl-8 pl-3 "
-          >
-            User: {message.content}
-          </div>
-        );
-      case "assistant":
-        return (
-          <div
-            key={index}
-            className="w-full rounded-lg h-fit border-accent text-accent py-4 lg:pl-8 pl-3 bg-secondary"
-          >
-            Assistant:{" "}
-            {chat[chat.length - 1] === message
-              ? assistantData
-              : message.content}
-          </div>
-        );
-    }
-  });
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Prevent the form from actually submitting and reloading the page
-    !isLoading && sendData(); // Trigger the sendChatData function
-  };
+import Messages from "@/app/modules/prototype/chat/Message";
+import ChatInput from "@/app/modules/prototype/chat/ChatInput";
+const Chat = () => {
   return (
     <div
-      className="space-y-10 select-none overflow-x-hidden overflow-y-scroll px-10 bg-transparent mx-auto box-border max-w-10xl
+      className="select-none overflow-x-hidden flex items-center border flex-col overflow-y-auto  px-10 bg-transparent mx-auto box-border max-w-10xl
 w-full my-32 h-fit"
     >
-      <div className="text-accent flex-col bg-transparent w-3/4   mx-auto">
-        {result}
-      </div>
-      <div className="w-10/12 h-10 select-none border backdrop-blur-md flex items-center overflow-hidden  border-accent outline-none rounded-lg absolute bottom-[10%] right-[8.3%]">
-        <form className=" w-full h-full" onSubmit={(e) => handleSubmit(e)}>
-          <input
-            onChange={(e) => {
-              setInputData(e.target.value);
-            }}
-            value={userInputData}
-            placeholder="I want to learn..."
-            className="bg-transparent xl:w-10/12 md:w-[80%] lg:w-10/12 sm:w-9/12 w-8/12 h-full px-2 text-accent box-border outline-none focus:border-text"
-          />
-          <button
-            type="submit"
-            className="text-text bg-secondary duration-200 border border-accent opacity-70 hover:opacity-100 transition-all w-[100px] xl:ml-6 lg:ml-4 md:ml-4 sm:text-sm h-2/3 rounded-lg box-border"
-          >
-            Send
-          </button>
-        </form>
-      </div>
+      <Messages />
+      <ChatInput />
     </div>
   );
 };
---
+++ app/modules/prototype/chat/ChatInput.tsx
@@ -0,0 +1,33 @@
+import useChatStore from "@/app/shared/storages/chatStorage";
+
+let ChatInput = () => {
+  const { isLoading, sendData, setInputData, userInputData } = useChatStore();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    !isLoading && sendData();
+  };
+  return (
+    <div className="w-3/4 max-w-[500px] fixed bottom-[8%] h-10 select-none border backdrop-blur-md flex items-center overflow-hidden  border-accent outline-none rounded-lg ">
+      <form
+        className=" w-full h-full flex items-center"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input
+          onChange={(e) => {
+            setInputData(e.target.value);
+          }}
+          value={userInputData}
+          placeholder="I want to learn..."
+          className="bg-transparent w-[90%] px-2 text-accent box-border outline-none focus:border-text"
+        />
+        <button
+          type="submit"
+          className="text-text bg-secondary duration-200 border border-accent opacity-70 hover:opacity-100 transition-all w-[10%] mx-2 sm:text-sm h-2/3 rounded-lg box-border"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+export default ChatInput;
