b0ac9541b7ea79d873257ef4b2cbe9aaa95c538b
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-01 23:38:35 +0300

    working on auth

--- app/(pages)/prototype/layout.tsx
+++ app/(pages)/prototype/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${roboto.variable} overflow-auto`}>
+    <html lang="en" className={`${roboto.variable} overflow-auto `}>
       <body>
         <HomeButton />
         {children}
--- app/components/footerComponents/themeSwitcher/ThemeSwitcher.tsx
+++ app/components/footerComponents/themeSwitcher/ThemeSwitcher.tsx
@@ -1,34 +1,23 @@
 "use client";
-import { useEffect } from "react";
 import useTheme from "@/app/shared/storages/themeStorage";
 import { BsSun, BsMoon } from "react-icons/bs";
 import { FiMonitor } from "react-icons/fi";
 let Switcher = () => {
-  const theme = useTheme((state) => state.theme);
-  const setTheme = useTheme((state) => state.setTheme);
-  let handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {};
+  const { theme, setTheme } = useTheme();
   let position;
   switch (theme) {
     case "light":
       position = "left-[4%]";
       break;
     case "dark":
-      position = "left-[37%]";
+      position = "left-[36.7%]";
       break;
     case "system":
       position = "left-[70.4%]";
       break;
-
-    default:
-      break;
   }
   return (
-    <div
-      onClick={(e) => {
-        handleClick(e);
-      }}
-      className=" border-accent border select-none flex items-center relative justify-around rounded-full w-24 h-8"
-    >
+    <div className=" border-accent border select-none flex items-center relative justify-around rounded-full w-24 h-8">
       <div
         className={`aspect-square -z-10 w-6 ${position} transition-all duration-150 bg-secondary rounded-full absolute`}
       />
