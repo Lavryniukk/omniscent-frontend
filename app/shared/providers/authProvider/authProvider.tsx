e4ea232af3b5cc83c616eb7dc5c43cb7d2808e24
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-02 02:36:48 +0300

    Save commit

--- app/shared/providers/authProvider/authProvider.tsx
+++ app/shared/providers/authProvider/authProvider.tsx
@@ -1,12 +1,24 @@
-import { createContext, useState, useEffect } from "react";
+"use client";
+import Cookies from "js-cookie";
+import { redirect } from "next/navigation";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext({});
-
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState(null);
-
-  const login = () => {}; // auth login function
-  const logout = () => {}; // auth logout function
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    let userToken = Cookies.get("token");
+    console.log(userToken);
+  }, []);
+  const login = (accessToken: string) => {
+    setUser({ token: accessToken });
+  }; // auth login function
+  const logout = () => {
+    redirect("https://omniscient-backend.onrender.com/user/logout");
+  }; // auth logout function
 
   return (
     <AuthContext.Provider
