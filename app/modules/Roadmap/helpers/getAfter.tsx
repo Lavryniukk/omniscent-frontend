7f3c5c3ee275700aa611614fdc2ecc1c3afd4892
Ivan Larin <n.larivanko@gmail.com>
2023-10-15 22:52:18 +0300

    fixed after before being not at their place

--- app/modules/Roadmap/helpers/getAfter.tsx
+++ app/modules/Roadmap/helpers/getAfter.tsx
@@ -2,12 +2,14 @@ import { RoadmapObject } from "../types/roadmapObject";
 import { RoadmapObjectArray } from "../types/RoadmapObjectArray";
 
 let getAfter = (node?: RoadmapObject, array?: RoadmapObjectArray) => {
-  let afterStyle =
+  let beforeStyle =
     "after:h-[2px] after:absolute after:w-1/2 after:bg-primary after:top-1/2 after:right-[80px]";
+
   if (array && node == array[0]) {
-    afterStyle = "";
+    beforeStyle = "";
   }
-  return afterStyle;
+
+  return beforeStyle;
 };
 
 export default getAfter;
