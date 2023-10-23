1bfbd5f157dced846dc84763416e6015d2f96863
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-24 00:06:17 +0300

    Added search section for projects/new

---
+++ app/modules/NewProjectField/components/ProjectsSearch/ProjectSearch.tsx
@@ -0,0 +1,73 @@
+"use client";
+import ProjectContainer from "@/app/modules/ProjectList/components/ProjectContainer/ProjectContainer";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+let arr = [
+  { title: "React front-end developer", _id: "1" },
+  { title: "Nest js back-end developer", _id: "2" },
+  { title: "Python back-end developer", _id: "3" },
+  { title: "Marketing scientist", _id: "4" },
+  { title: "Data scientist", _id: "5" },
+  { title: "DevOps", _id: "6" },
+];
+export default function ProjectSearch() {
+  const [text, setText] = useState("");
+  const [filteredArray, setFilteredArray] = useState(arr);
+
+  let handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+
+    const regex = new RegExp(e.target.value, "i");
+    const filtered = arr.filter((item) => regex.test(item.title));
+    setFilteredArray(filtered);
+  };
+
+  const highlightText = (str: string, term: string) => {
+    const splitText = str.split(new RegExp(`(${term})`, "gi"));
+    return splitText.map((chunk, index) =>
+      chunk.toLowerCase() === term.toLowerCase() ? (
+        <span key={index} className="bg-primary-800">
+          {chunk}
+        </span>
+      ) : (
+        chunk
+      )
+    );
+  };
+
+  return (
+    <div>
+      <div className="relative h-10 w-full mx-auto items-center mt-20 flex">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+          className=" bg-secondary focus:outline-none h-full focus:border-text w-full rounded-lg border-accent text-accent pl-3 border mx-auto "
+        />
+        <AiOutlineSearch
+          size={20}
+          className="absolute text-accent bg-secondary right-3"
+        />
+      </div>
+      <div className="w-full h-fit space-y-3 overflow-hidden mt-10">
+        {filteredArray.length ? (
+          filteredArray.map(
+            (item, index) =>
+              index <= 4 && (
+                <ProjectContainer
+                  title={highlightText(item.title, text)}
+                  key={item._id}
+                ></ProjectContainer>
+              )
+          )
+        ) : (
+          <div className="text-accent text-lg  mx-auto text-center ">
+            Oops, seems like nothing was found
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- app/modules/ProjectList/ProjectList.tsx
+++ app/modules/ProjectList/ProjectList.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import Link from "next/link";
 import ProjectContainer from "./components/ProjectContainer/ProjectContainer";
 
 let arr = [
-  { title: "Next js front-end developer" },
-  { title: "Next js front-end developer" },
-  { title: "Next js front-end developer" },
+  { title: "Next js front-end developer", _id: "1" },
+  { title: "Next js front-end developer", _id: "2" },
+  { title: "Next js front-end developer", _id: "3" },
 ];
 let ProjectList = () => {
-  let res = arr.map((elem) => <ProjectContainer title={elem.title} />);
+  let res = arr.map((elem) => (
+    <ProjectContainer key={elem._id} title={elem.title} />
+  ));
   return (
     <div className="mx-auto w-1/3 max-w-[600px] min-w-[500px] p-5 font-inter h-[800px] border-2 border-secondary rounded-2xl">
       <h1 className="text-4xl text-center font-bold mx-auto text-text trancking-tight mt-10 font-inter">
@@ -17,8 +20,13 @@ let ProjectList = () => {
       <div className="w-3/4 mx-auto border-y border-accent space-y-5 p-5 h-fit mt-20">
         {res}
       </div>
-      <p className="mx-auto text-center underline-offset-2 select-none underline text-accent-600 mt-10">
-        Create new project
+      <p className="mx-auto  text-center underline-offset-2 select-none underline text-accent-600 mt-10">
+        <Link
+          href="/projects/new"
+          className="hover:text-accent transition-colors duration-200"
+        >
+          Create new project
+        </Link>
       </p>
     </div>
   );
