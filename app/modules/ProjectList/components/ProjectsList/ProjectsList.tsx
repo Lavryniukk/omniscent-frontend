6583bea7fac7de0e404fc6d445f351f8a16356a0
Ivan Larin <n.larivanko@gmail.com>
2023-10-25 21:02:15 +0300

    added appropriate output for user projects, minot changes

---
+++ app/modules/ProjectList/components/ProjectsList/ProjectsList.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import ProjectContainer from "../ProjectContainer/ProjectContainer";
+
+export default function ProjectsList({ projects }: { projects: Array<any> }) {
+  return (
+    <>
+      <div className="w-3/4 mx-auto space-y-5 p-5 h-fit py-20">
+        {projects.map((elem) => (
+          <ProjectContainer key={elem._id} title={elem.title} />
+        ))}
+      </div>
+      <p className="mx-auto text-center underline-offset-2 select-none underline text-accent-600">
+        <Link
+          href="/projects/new"
+          className="hover:text-accent transition-colors duration-200"
+        >
+          Create new project
+        </Link>
+      </p>
+    </>
+  );
+}
