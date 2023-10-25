4fad5e41a46f8fbc73e62041f8390844c792178a
Ivan Larin <n.larivanko@gmail.com>
2023-10-25 21:02:15 +0300

    added appropriate output for user projects, minot changes

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ProjectList from "@/app/modules/ProjectList/ProjectList";
+import UserProjects from "@/app/modules/ProjectList/ProjectList";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 
 import { useQuery } from "@tanstack/react-query";
@@ -7,7 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 function PrototypePage() {
   return (
     <div className="flex h-screen border-accent items-center">
-      <ProjectList />
+      <UserProjects />
     </div>
   );
 }
--- app/modules/ProjectList/ProjectList.tsx
+++ app/modules/ProjectList/ProjectList.tsx
@@ -1,34 +1,21 @@
 "use client";
+import ProjectsList from "./components/ProjectsList/ProjectsList";
+// import { AiOutlineQuestion } from "react-icons/ai";
+import ProjectsNotFound from "./components/ProjectsNotFound/ProjectsNotFound";
 
-import Link from "next/link";
-import ProjectContainer from "./components/ProjectContainer/ProjectContainer";
+let arr: Array<any> = [];
 
-let arr = [
-  { title: "Next js front-end developer", _id: "1" },
-  { title: "Next js front-end developer", _id: "2" },
-  { title: "Next js front-end developer", _id: "3" },
-];
-let ProjectList = () => {
-  let res = arr.map((elem) => (
-    <ProjectContainer key={elem._id} title={elem.title} />
-  ));
+export default function UserProjects() {
   return (
-    <div className="mx-auto w-1/3 max-w-[600px] min-w-[500px] p-5 font-inter h-[800px] border-2 border-secondary rounded-2xl">
-      <h1 className="text-4xl text-center font-bold mx-auto text-text trancking-tight mt-10 font-inter">
+    <div className="mx-auto w-1/3 max-w-[600px] min-w-[500px] px-5 py-16 font-inter h-fit  border-2 border-secondary rounded-2xl">
+      <h1 className="text-4xl text-center font-bold mx-auto text-text trancking-tight font-inter">
         Your learning projects
       </h1>
-      <div className="w-3/4 mx-auto border-y border-accent space-y-5 p-5 h-fit mt-20">
-        {res}
-      </div>
-      <p className="mx-auto  text-center underline-offset-2 select-none underline text-accent-600 mt-10">
-        <Link
-          href="/projects/new"
-          className="hover:text-accent transition-colors duration-200"
-        >
-          Create new project
-        </Link>
-      </p>
+      {arr.length !== 0 ? (
+        <ProjectsList projects={arr} />
+      ) : (
+        <ProjectsNotFound />
+      )}
     </div>
   );
-};
-export default ProjectList;
+}
