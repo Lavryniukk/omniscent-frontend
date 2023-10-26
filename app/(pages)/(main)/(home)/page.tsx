aad11f88668bfa59a5dd899db21272917a39b1ab
Ivan Larin <n.larivanko@gmail.com>
2023-10-26 18:00:27 +0300

    added comments for HomePage

--- app/(pages)/(main)/(home)/page.tsx
+++ app/(pages)/(main)/(home)/page.tsx
@@ -3,17 +3,16 @@ import Title from "@/app/modules/Hero/components/Title/Title";
 import Reviews from "@/app/modules/Reviews/Reviews";
 import { NextPage } from "next";
 
+// HomePage is a Next.js page component representing the main landing page of the application.
+// It includes the Title component, Features component, and Reviews component.
 let HomePage: NextPage = () => {
   return (
-    <main
-      className="mb-10 mt-14 overflow-hidden pt-40  h-fit bg-transparent mx-auto box-border max-w-10xl
-    w-full "
-    >
-      <Title />
-      <Features />
-
-      <Reviews />
+    <main className="mb-10 mt-14 overflow-hidden pt-40  h-fit bg-transparent mx-auto box-border max-w-10xl w-full">
+      <Title /> {/* Renders the title section of the landing page. */}
+      <Features /> {/* Renders the features section of the landing page. */}
+      <Reviews /> {/* Renders the reviews section of the landing page. */}
     </main>
   );
 };
+
 export default HomePage;
--- app/modules/Reviews/components/ReviewCard/ReveiwCard.tsx
+++ app/modules/Reviews/components/ReviewCard/ReveiwCard.tsx
@@ -12,7 +12,10 @@ let ReviewCard: React.FC<ReviewCardProps> = ({
   return (
     <div className="lg:w-1/3 md:w-full max-h-54 border border-secondary-600 text-text p-5 rounded-lg space-y-5">
       <div className="flex justify-between items-center">
-        <div>{RatingAssembler({ amount: rating })}</div>
+        <div>
+          {/* Render the rating using the RatingAssembler function */}
+          {RatingAssembler({ amount: rating })}
+        </div>
 
         <div className="flex justify-between items-center gap-2 ">
           <Image
@@ -21,16 +24,20 @@ let ReviewCard: React.FC<ReviewCardProps> = ({
             className="w-[32px] h-[32px] rounded-full bg-center bg-contain"
             src={imageUrl}
             alt="Review profile picture"
-          />
+          />{" "}
+          {/* Display the reviewer's profile picture using the Next.js Image component */}
           <div>
-            <div className="text-lg">{name}</div>
-            <div className="text-accent text-sm">@{nickname}</div>
+            <div className="text-lg">{name}</div>{" "}
+            {/* Display the reviewer's name */}
+            <div className="text-accent text-sm">@{nickname}</div>{" "}
+            {/* Display the reviewer's nickname */}
           </div>
         </div>
       </div>
 
       <div className="space-y-2">
-        <p className="text-accent">{content}</p>
+        <p className="text-accent">{content}</p>{" "}
+        {/* Display the review content */}
       </div>
     </div>
   );
