36403430a47f6e93f5260598c36a18fcfe6a6577
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-27 16:11:54 +0300

    Fixed gradient on main page, minor changes

--- app/modules/Hero/components/Title/Title.tsx
+++ app/modules/Hero/components/Title/Title.tsx
@@ -3,16 +3,7 @@ import GetStartedButton from "../GetStartedButton/GetStartedButton";
 // Component for rendering the title section
 let Title = () => {
   return (
-    <div className="  mx-2 overflow-visible overflow-x-clip py-20 items-center relative flex-col justify-center space-y-10   ">
-      {/* Commented out visual elements */}
-      {/* These elements create decorative visual effects and are currently commented out. They can be uncommented for visual enhancements. */
-      /*
-      <div className=" border-r rotate-45 w-10 h-20 absolute -top-[84px] -left-1 border-dashed border-secondary-700"></div>
-      <div className=" rotate-45 border-b w-20 h-10 absolute -top-[104px] -right-[26px] border-dashed border-secondary-700"></div>
-      <div className=" border-r -rotate-45 w-10 h-20 absolute -bottom-[84px] -left-1 border-dashed border-secondary-700"></div>
-      <div className=" border-b -rotate-45  w-20 h-10 absolute -bottom-[36px] right-0.5 border-dashed border-secondary-700"></div>
-      */}
-
+    <div className="  overflow-visible overflow-x-clip py-20 items-center relative flex-col justify-center space-y-10 max-w-10xl mx-auto">
       {/* Main title */}
       <h1
         className="observe transition delay-0 duration-500 bg-gradient-to-t from-text flex items-center w-10/12 justify-center antialiased px-5 to-text-500  mx-auto leading-none
@@ -20,8 +11,6 @@ let Title = () => {
       >
         The AI solution for self-education
       </h1>
-      {/* Horizontal rule */}
-      {/* <div className="w-full border-t border-secondary-700 border-dashed h-1 g-gradient-to-t " /> */}
 
       {/* Description text */}
       <p className="observe transition delay-100 duration-500 text-accent hyphens-auto leading-relaxed mt-40 mx-auto font-light flex items-center justify-center w-3/4 text-[max(15px,min(2vw,20px))] text-center ">
