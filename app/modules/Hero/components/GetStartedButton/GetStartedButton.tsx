58c0a50756ffd89673241728acb860ce6bcc4c22
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-27 19:40:20 +0300

    Experimenting on title

--- app/modules/Hero/components/GetStartedButton/GetStartedButton.tsx
+++ app/modules/Hero/components/GetStartedButton/GetStartedButton.tsx
@@ -6,7 +6,7 @@ let GetStartedButton = () => {
     <div className="delay-200 observe transition-all duration-500">
       <Link
         // Styling for the button
-        className="w-fit hover:border-accent tracking-tight border-[0.2px] hover:text-text border-accent text-md font-medium hover:bg-background transition-all duration-500  box-border mx-auto text-background bg-text  px-5 py-1  rounded-lg block  text-[min(2vw,20px)]"
+        className="w-fit hover:border-accent tracking-tight border-[0.5px] hover:text-text border-accent text-md font-medium hover:bg-background transition-all duration-500  box-border mx-auto text-background bg-text  px-5 py-1  rounded-lg block  text-[min(3vw,20px)]"
         href="/"
       >
         Get started
