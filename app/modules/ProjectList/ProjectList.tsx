49d763dda9912e207412f58db224b77af8ad1aab
Ivan Larin <n.larivanko@gmail.com>
2023-10-28 16:58:42 +0300

    added useQuery for use projects

--- app/modules/ProjectList/ProjectList.tsx
+++ app/modules/ProjectList/ProjectList.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import ProjectsList from "./components/ProjectsList/ProjectsList";
 // import { AiOutlineQuestion } from "react-icons/ai";
@@ -7,11 +8,11 @@ import ProjectsNotFound from "./components/ProjectsNotFound/ProjectsNotFound";
 let arr: Array<any> = [{ title: "Node.js", _id: "1" }];
 
 export default function UserProjects() {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  const handleToggle = () => {
-    setIsLoading((prev) => !prev);
-  };
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { data, isLoading } = useQuery({
+    queryKey: ["userProjects"],
+    queryFn: () => fetch("/user/roadmaps/"),
+  });
   return (
     <div className="mx-auto w-1/3 max-w-[600px] min-w-[500px] px-5 py-16 font-inter h-fit border-2 border-secondary rounded-2xl">
       <h1 className="text-4xl text-center font-bold mx-auto text-text trancking-tight font-inter">
