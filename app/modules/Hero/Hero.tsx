51cd4b8297da55dc6fa2b115483319aa0665d29a
Lavryniuk Lev <leoyt109@gmail.com>
2023-10-30 21:02:28 +0200

    Switched to h-screen in home page

--- app/modules/Hero/Hero.tsx
+++ app/modules/Hero/Hero.tsx
@@ -1,9 +1,11 @@
-import TypingAnimation from "@/app/modules/Hero/components/TypingAnimation/TypingAnimation";
-import GetStartedButton from "@/app/modules/Hero/components/GetStartedButton/GetStartedButton";
-import Gradient from "@/app/modules/Hero/components/Gradient/Gradient";
 import Title from "./components/Title/Title";
 
 let Home = () => {
-  return <div></div>;
+  return (
+    <div className=" min-h-screen max-h-fit relative border-b border-secondary overflow-hidden">
+      <Title />
+      <div className="absolute bottom-0 bg-gradient-to-t from-primary-950 to-background blur-3xl w-full h-16"></div>
+    </div>
+  );
 };
 export default Home;
