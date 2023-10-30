f8b837d76f844478e7e4ac8cdd6eb43b5ed52d77
Ivan Larin <n.larivanko@gmail.com>
2023-10-30 21:56:00 +0200

    updated links in projects

--- app/modules/ProjectList/ProjectList.tsx
+++ app/modules/ProjectList/ProjectList.tsx
@@ -30,7 +30,7 @@ export default function UserProjects() {
           </div>
           <p className="mx-auto text-center underline-offset-2 select-none underline text-accent-600">
             <Link
-              href="/projects/new"
+              href="/projects/chat"
               className="hover:text-accent transition-colors duration-200"
             >
               Create new project
