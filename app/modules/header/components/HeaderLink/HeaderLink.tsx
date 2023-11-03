6622d2a597c57a95415cf631b37333d54aea0b2d
Ivan Larin <n.larivanko@gmail.com>
2023-11-03 21:33:32 +0200

    header bug fix

--- app/modules/header/components/HeaderLink/HeaderLink.tsx
+++ app/modules/header/components/HeaderLink/HeaderLink.tsx
@@ -9,7 +9,7 @@ let HeaderLink = ({ url, name }: Props) => {
   return (
     <a
       href={url} // Set the URL to navigate to based on the prop.
-      className="text-text hover:text-accent text-md font-inter font-normal transition-colors duration-200 text-2xl"
+      className="text-text hover:text-accent text-md font-inter font-normal sm:font-light transition-colors duration-200 text-2xl sm:text-lg"
     >
       {name} {/* Display the link name or label as the link text. */}
     </a>
