b9652b6668871d7494cd82a42529fb3a08f2c0ab
Lavryniuk Lev <leoyt109@gmail.com>
2023-11-04 15:03:41 +0200

    Added accesstocen to console

--- app/(pages)/(main)/(home)/page.tsx
+++ app/(pages)/(main)/(home)/page.tsx
@@ -2,10 +2,16 @@
 import Features from "@/app/UI/static/Home/Features/Features";
 import Hero from "@/app/UI/static/Home/Hero/Hero";
 import Reviews from "@/app/UI/static/Home/Reviews/Reviews";
+import api from "@/app/modules/ProjectList/api/healthCheck";
+import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
+import { useEffect } from "react";
 // HomePage is a Next.js page component representing the main landing page of the application.
 // It includes the Title component, Features component, and Reviews component.
 let HomePage: NextPage = () => {
+  useEffect(() => {
+    api();
+  }, []);
   return (
     <main className="mb-10  overflow-hidden  h-fit bg-transparent mx-auto box-border w-full">
       <Hero /> {/* Renders the title section of the landing page. */}
