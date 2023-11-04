7da4d95dc72d2bd4667ce427f72b1036f3058b2d
Ivan Larin <n.larivanko@gmail.com>
2023-11-04 20:13:17 +0200

    bug fix

--- app/UI/static/Home/Hero/Hero.tsx
+++ app/UI/static/Home/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import Title from "./components/Title/Title";
 
 let Home = () => {
   return (
-    <div className=" min-h-screen max-h-fit relative border-b border-secondary overflow-hidden">
+    <div className="h-screen max-h-[1440px] relative border-b border-secondary overflow-hidden">
       <Title />
       <div className="absolute bottom-0 bg-gradient-to-t from-primary-950 to-background blur-3xl w-full h-16"></div>
     </div>
