57f8801b63898e65adfbf1a94dd24b3247e93754
Ivan Larin <n.larivanko@gmail.com>
2023-11-09 20:54:27 +0200

    added responsiveness for profile

--- app/modules/Profile/Profile.tsx
+++ app/modules/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { fetchProjects } from "./api/fetchProjects";
 import { Project } from "./types/project";
 
@@ -20,7 +20,7 @@ export default function ProfilePage() {
 
   console.log("Projects:", projects);
 
-  const arr = [
+  const arr: [] | Project[] = [
     // {
     //   _id: "new_id_0",
     //   owner_id: "google-oauth2|116000322186574711788",
@@ -100,66 +100,95 @@ export default function ProfilePage() {
   }
 
   return (
-    <div className="bg-background mx-auto mt-20 px-2 h-screen min-w-[420px] sm:container ">
-      <div className="h-fit w-full flex justify-between gap-4">
-        <div className="space-y-10 min-w-[400px] w-1/4">
+    <div className="bg-background mx-auto mt-20 px-2 h-screen sm:min-w-[420px] sm:container ">
+      <div className="h-fit w-full flex flex-col gap-10 sm:flex-row justify-between sm:gap-4">
+        <div className="space-y-10 sm:min-w-[300px] w-full mx-auto sm:w-1/4">
           <div className="flex flex-col items-start bg-secondary p-6 rounded-lg shadow-lg">
             <img
               src={pic}
               alt="User profile"
               className="rounded-full border-4 border-accent w-40 h-40"
             />
-            <h2 className="mt-4 text-3xl font-bold text-text">{user.name}</h2>
-            <p className="text-sm text-accent">@{user.nickname}</p>
+            <h2 className="mt-4 text-4xl sm:text-3xl font-bold text-text">
+              {user.name}
+            </h2>
+            <p className="text-base sm:text-sm text-accent">@{user.nickname}</p>
           </div>
-          <div className=" w-full max-w-xl">
+          <div className=" w-full sm:max-w-xl">
             <form className="bg-secondary p-6 rounded-lg shadow-lg">
               <div className="">
-                <label htmlFor="bio" className="block text-lg text-text mb-2">
+                <label
+                  htmlFor="bio"
+                  className="block text-xl sm:text-lg text-text mb-2"
+                >
                   Bio
                 </label>
                 <textarea
                   id="bio"
                   rows={3}
-                  className="w-full p-2 rounded min-h-[40px] h-fit max-h-[150px] bg-background text-text"
+                  className="w-full p-2 rounded min-h-[40px] text-lg sm:text-base h-fit max-h-[150px] bg-background text-text"
                   placeholder="Tell us your story"
                 ></textarea>
               </div>
             </form>
           </div>
           <div className="p-6 bg-secondary  rounded-lg">
-            <h2 className="text-text text-lg mb-2">Achievments</h2>
+            <h2 className="text-text text-xl sm:text-lg mb-2">Achievments</h2>
             <div className="h-20 bg-background rounded w-full flex "></div>
           </div>
         </div>
-        <div className="w-8/12 border-secondary flex flex-col overflow-hidden rounded-lg">
+        <div className="w-full sm:w-8/12 mx-auto border-secondary flex flex-col overflow-hidden rounded-lg">
           <div className="h-20 w-full bg-background flex items-center justify-center text-center ">
-            <p className="font-semibold text-text text-3xl">{`${user.name}'s Projects`}</p>
+            <p className="font-semibold text-text text-4xl sm:text-3xl">{`${user.name}'s Projects`}</p>
           </div>
-          {/* {projects ? (
-            <div className="h-fit grid grid-cols-2 p-1 gap-4">
-              {projects.map((item) => (
-                <Link
-                  href={"/"}
-                  key={item._id}
-                  className="border rounded border-accent p-8"
-                >
-                  <div className="text-accent text-2xl text-center">
-                    {item.title}
-                  </div>
-                </Link>
-              ))}
+          {projects && projects.length > 0 ? (
+            <div className="h-fit grid grid-cols-1 lg:grid-cols-2 p-1 gap-4">
+              {projects.map((item, index, array) => {
+                if (index !== array.length - 1) {
+                  return (
+                    <Link
+                      href={"/"}
+                      key={item._id}
+                      className="border rounded border-accent p-6"
+                    >
+                      <div className="text-accent text-2xl text-center">
+                        {item.title}
+                      </div>
+                    </Link>
+                  );
+                } else
+                  return (
+                    <Fragment key={item._id}>
+                      <Link
+                        href={"/"}
+                        className="border rounded border-accent p-6 hover:border-accent-300 transition duration-200"
+                      >
+                        <div className="text-accent text-2xl text-center">
+                          {item.title}
+                        </div>
+                      </Link>
+                      <Link
+                        href={"/create"}
+                        className="border rounded bg-text text-background p-6 hover:bg-background hover:text-text transition duration-300"
+                      >
+                        <div className="text-inherit text-2xl text-center font-medium">
+                          Create new
+                        </div>
+                      </Link>
+                    </Fragment>
+                  );
+              })}
             </div>
-          ) : ( */}
-          <div className="h-full text-text flex w-full justify-center items-center">
-            <Link
-              href={"/projects/create"}
-              className="hover:bg-text hover:text-background font-medium transition duration-300 text-2xl block border border-text p-4 rounded"
-            >
-              {`Embark on the journey ->`}
-            </Link>
-          </div>
-          {/* )} */}
+          ) : (
+            <div className="h-full text-text flex w-full justify-center items-center">
+              <Link
+                href={"/projects/create"}
+                className="hover:bg-text hover:text-background font-medium transition duration-300 text-base md:text-2xl block border border-text p-3 md:p-4 rounded"
+              >
+                {`Embark on the journey ->`}
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
