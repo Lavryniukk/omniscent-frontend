58a42eaa8727ca0094d291bc2be392bdab1c2115
Ivan Larin <n.larivanko@gmail.com>
2023-11-09 18:15:54 +0200

    refactored submit form

--- app/modules/NewProjectForm/NewProjectForm.tsx
+++ app/modules/NewProjectForm/NewProjectForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FormInput from "./components/FormInput/FormInput";
 import FormSelect from "./components/FormSelect/FormSelect";
 import FormSubmit from "./components/FormSubmit/FormSubmit";
@@ -9,40 +9,44 @@ import sendData from "./helpers/sendData";
 
 export default function NewProjectForm() {
   const [formData, setFormData] = useState<FormState>({
-    inputData: null,
+    inputData: "",
     selectData: null,
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    sendData(formData);
+    setFormData((prev) => ({ ...prev, inputData: "" }));
+
+    console.log("Submit");
+
+    // sendData(formData);
   };
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData((prev) => ({ ...prev, inputData: e.target.value }));
   };
 
-  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setFormData((prev) => ({ ...prev, selectData: e.target.value }));
-  };
+  // const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   setFormData((prev) => ({ ...prev, selectData: e.target.value }));
+  // };
 
   return (
-    <div className="w-fit h-fit container border mx-auto font-inter rounded-lg border-secondary px-8 max-w-[600px] py-12 ">
-      <form
-        className="flex flex-col space-y-16 h-full"
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <h2 className="text-text text-center text-3xl font-bold">
-          Help us, help you!
-        </h2>
+    <form
+      className="flex flex-col items-center space-y-24 w-full h-fit max-w-[500px] container border justify-center xs:mx-auto font-inter rounded-lg border-secondary px-8 py-12 mx-3"
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      <h2 className="text-text text-center text-3xl font-bold">
+        Help us, help you!
+      </h2>
 
-        <FormInput handleFunction={handleInput} />
+      {/* <div className="flex flex-col gap-16"> */}
+      <FormInput handleFunction={handleInput} inputData={formData.inputData} />
 
-        <FormSelect handleFunction={handleSelect} />
+      {/* <FormSelect handleFunction={handleSubmit} /> */}
+      {/* </div> */}
 
-        <FormSubmit />
-      </form>
-    </div>
+      <FormSubmit />
+    </form>
   );
 }
