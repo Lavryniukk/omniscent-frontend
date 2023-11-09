d906bb59e28377e8b83274d81fc665b2bb3b854c
Ivan Larin <n.larivanko@gmail.com>
2023-11-09 18:31:43 +0200

    bug fix

--- app/modules/NewProjectForm/NewProjectForm.tsx
+++ app/modules/NewProjectForm/NewProjectForm.tsx
@@ -33,11 +33,11 @@ export default function NewProjectForm() {
 
   return (
     <form
-      className="flex flex-col items-center space-y-24 w-full h-fit max-w-[500px] container border justify-center xs:mx-auto font-inter rounded-lg border-secondary px-8 py-12 mx-3"
+      className="flex flex-col items-center space-y-16 xs:space-y-24 w-full h-fit max-w-[500px] container xs:border justify-center xs:mx-auto font-inter rounded-lg xs:border-secondary px-4 py-8 xs:px-8 xs:py-12 mx-3"
       onSubmit={(e) => handleSubmit(e)}
     >
-      <h2 className="text-text text-center text-3xl font-bold">
-        Help us, help you!
+      <h2 className="text-text text-center text-4xl sm:text-5xl font-bold">
+        Create roadmap
       </h2>
 
       {/* <div className="flex flex-col gap-16"> */}
