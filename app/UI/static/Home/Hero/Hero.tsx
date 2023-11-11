23a36f423b12a72900a7dc4fd8793a2067690eea
Lavryniuk Lev <leoyt109@gmail.com>
2023-11-11 13:40:39 +0200

    save commit

--- app/UI/static/Home/Hero/Hero.tsx
+++ app/UI/static/Home/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import Title from "./components/Title/Title";
 
 let Home = () => {
   return (
-    <div className="section relative border-b border-secondary overflow-hidden">
+    <div className=" lg:py-20 py-12 relative border-b overflow-hidden">
       <Title />
       <div className="absolute bottom-0 bg-gradient-to-t from-primary-950 to-background blur-3xl w-full h-16"></div>
     </div>
