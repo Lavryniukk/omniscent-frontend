5eef18aacfe3a7b1090467d764dfeb8ca3f37f60
Lavryniukk <leoyt109@gmail.com>
2023-11-11 09:36:02 +0200

    ch

--- app/(pages)/(main)/(home)/page.tsx
+++ app/(pages)/(main)/(home)/page.tsx
@@ -7,12 +7,13 @@ import { NextPage } from "next";
 import { useEffect } from "react";
 // HomePage is a Next.js page component representing the main landing page of the application.
 // It includes the Title component, Features component, and Reviews component.
+
 let HomePage: NextPage = () => {
-  useEffect(() => {
-    api();
-  }, []);
+  // useEffect(() => {
+  //   api();
+  // }, []);
   return (
-    <main className="mb-10 overflow-hidden  h-fit bg-transparent mx-auto box-border w-full">
+    <main className="mb-10  overflow-hidden  h-fit bg-transparent mx-auto box-border w-full">
       <Hero /> {/* Renders the title section of the landing page. */}
       <Features /> {/* Renders the features section of the landing page. */}
       <Reviews /> {/* Renders the reviews section of the landing page. */}
