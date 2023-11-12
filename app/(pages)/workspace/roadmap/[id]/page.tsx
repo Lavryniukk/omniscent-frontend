d44a952b23ab381cb3db8088c80e0afb62ab42d3
Ivan Larin <n.larivanko@gmail.com>
2023-11-12 21:16:08 +0200

    implemented isComplete

--- app/(pages)/workspace/roadmap/[id]/page.tsx
+++ app/(pages)/workspace/roadmap/[id]/page.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import Roadmap from "@/app/modules/ProjectList/types/Roadmap";
-import { useEffect, useState } from "react";
 import fetchRoadmap from "../fetchRoadmap";
 import { useQuery } from "@tanstack/react-query";
+
 let arr = [
-  { title: "HTML" },
-  { title: "CSS" },
-  { title: "JavaScript" },
-  { title: "React" },
-  { title: "Redux" },
-  { title: "SASS" },
-  { title: "NPM" },
-  { title: "Webpack" },
-  { title: "Babel" },
-  { title: "Jest" },
+  { _id: "1", title: "HTML", isCompleted: true },
+  { _id: "2", title: "CSS", isCompleted: false },
+  { _id: "3", title: "JavaScript", isCompleted: false },
+  { _id: "4", title: "React", isCompleted: false },
+  { _id: "5", title: "Redux", isCompleted: false },
+  { _id: "6", title: "SASS", isCompleted: false },
+  { _id: "7", title: "NPM", isCompleted: false },
+  { _id: "8", title: "Webpack", isCompleted: false },
+  { _id: "9", title: "Babel", isCompleted: false },
+  { _id: "10", title: "Jest", isCompleted: false },
 ];
 
 export default function RoadmapPage({ params }: { params: { id: string } }) {
@@ -27,20 +26,44 @@ export default function RoadmapPage({ params }: { params: { id: string } }) {
       <div className=" w-fit mx-auto">
         <ul className="text-text mx-auto  w-fit py-20 h-fit  flex items-center justify-center flex-col ">
           {!error && data ? (
-            data.node_list.map(
-              (tech: { title: string; _id: string }, index: number) => {
+            arr.map(
+              (
+                tech: { title: string; _id: string; isCompleted: boolean },
+                index: number,
+                array
+              ) => {
+                let current;
+                const prevTech = array[index - 1];
+
+                if (index === 0 && !tech.isCompleted) {
+                  current = true;
+                } else {
+                  if (prevTech?.isCompleted && !tech.isCompleted) {
+                    current = true;
+                  } else current = false;
+                }
                 return (
                   <li
-                    key={index}
-                    className="w-full flex items-center justify-center flex-col min-w-[200px] "
+                    key={tech._id}
+                    className="w-full flex items-center justify-center flex-col min-w-[200px]"
                   >
                     <div
-                      className={`roadmap-node ${index <= 3 && "roadmap-done"}`}
+                      className={`text-center px-5 w-full cursor-pointer relative hover:text-background hover:bg-text duration-200 
+                      transition-colors font-semibold flex items-center justify-center border h-12 bg-background border-accent 
+                      rounded-lg ${
+                        tech.isCompleted &&
+                        "opacity-60 bg-secondary hover:bg-background hover:text-accent"
+                      } ${current && "drop-shadow-[0px_5px_5px_#f5f5f5]"}
+                      `}
                     >
                       {tech.title}
                     </div>
-                    {index != data.node_list.length - 1 && (
-                      <div className="roadmap-arrow" />
+                    {index != arr.length - 1 && (
+                      <div
+                        className={`block w-0.5 select-none h-20 bg-accent ${
+                          tech.isCompleted && "opacity-60"
+                        }`}
+                      />
                     )}
                   </li>
                 );
