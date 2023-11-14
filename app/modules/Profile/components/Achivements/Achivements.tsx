55ed9c5a7355309f979d55bec4b7e057f7438759
Ivan Larin <n.larivanko@gmail.com>
2023-11-14 18:45:44 +0200

    minoe changes

--- app/modules/Profile/components/Achivements/Achivements.tsx
+++ app/modules/Profile/components/Achivements/Achivements.tsx
@@ -4,7 +4,7 @@ export default function Achivements() {
   return (
     <div className="rounded-lg">
       <h2 className="text-text text-xl sm:text-lg mb-2">Achievments</h2>
-      <div className="h-20 bg-background border border-secondary rounded w-full flex "></div>
+      <div className="h-20 bg-background border border-secondary rounded w-3/4 sm:w-full flex "></div>
     </div>
   );
 }
