6e818fcb978dc119f1c8cdfb04b9985df0c5369a
Ivan Larin <n.larivanko@gmail.com>
2023-11-19 17:07:16 +0200

    implemented roadmap deletion

--- app/modules/ProjectList/components/ProjectContainer/fetchDelete.ts
+++ app/modules/ProjectList/components/ProjectContainer/fetchDelete.ts
@@ -3,10 +3,11 @@
 import { axiosWithAuth } from "@/app/shared/config/axiosConfig";
 
 const fetchDelete = async (roadmapId: string) => {
+  console.log();
   try {
     await axiosWithAuth({
       method: "DELETE",
-      url: `/users/me/roadmaps/:${roadmapId}`,
+      url: `/users/me/roadmaps/${roadmapId}`,
     });
   } catch (e) {
     console.log(e);
