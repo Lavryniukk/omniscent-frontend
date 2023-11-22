93cf88106dd6081b63324f045e3946564acf7e02
Lavryniukk <leoyt109@gmail.com>
2023-11-22 23:29:11 +0200

    added fancy looking menus in roadmap page

--- app/UI/buttons/primaryBtn/PrimaryBtn.tsx
+++ app/UI/buttons/primaryBtn/PrimaryBtn.tsx
@@ -12,29 +12,22 @@ export default function PrimaryBtn({
   callbackFn,
 }: ButtonProps) {
   return (
-    <div
+    <Link
       style={{
         width: `${width}`,
+
+        borderRadius: `${rounded}`,
+        height: `${height}`,
       }}
-      className={`${
-        observed && "delay-200 observe transition-all duration-500"
-      } ${classname}`}
+      onClick={(e) => {
+        !href && e.preventDefault();
+        callbackFn && callbackFn();
+      }}
+      // Styling for the button
+      className={`border ${observed} m-2 overflow-visible hover:text-text border-text text-md font-medium hover:bg-background transition-all duration-200 flex justify-center items-center  mx-auto text-background bg-text rounded-md ${classname}`}
+      href={`${href ? href : ""} `}
     >
-      <Link
-        style={{
-          borderRadius: `${rounded}`,
-          height: `${height}`,
-        }}
-        onClick={(e) => {
-          !href && e.preventDefault();
-          callbackFn && callbackFn();
-        }}
-        // Styling for the button
-        className={`border-2 w-full hover:text-text border-text text-md font-medium hover:bg-background transition-all duration-200 flex justify-center items-center box-border mx-auto text-background bg-text rounded-md text-lg`}
-        href={`${href ? href : ""}`}
-      >
-        {text}
-      </Link>
-    </div>
+      {text}
+    </Link>
   );
 }
