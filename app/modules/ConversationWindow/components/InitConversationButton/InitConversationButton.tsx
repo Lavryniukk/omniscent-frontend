5e5c8e99aa05e3523aaec5158ac2e945871fca71
Ivan Larin <n.larivanko@gmail.com>
2023-12-03 01:44:47 +0200

    bug fix

--- app/modules/ConversationWindow/components/InitConversationButton/InitConversationButton.tsx
+++ app/modules/ConversationWindow/components/InitConversationButton/InitConversationButton.tsx
@@ -16,31 +16,29 @@ export default function InitConversationButton({
   user_roadmap_id,
   node_title,
 }: InitConversationButtonProps) {
-  const isLoading = true;
-  // const { user } = useUser();
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ["initConversationUser"],
-  //   queryFn: async () => await fetchUserData(user?.sub as string),
-  // });
-  // console.log(data && data.user_metadata.bio.language);
-  // const initConversation = useConversationStorage(
-  //   (state) => state.initConversation
-  // );
+  const { user } = useUser();
+  const { data, isLoading } = useQuery({
+    queryKey: ["initConversationUser"],
+    queryFn: async () => await fetchUserData(user?.sub as string),
+  });
+  console.log(data && data.user_metadata.bio.language);
+  const initConversation = useConversationStorage(
+    (state) => state.initConversation
+  );
   return !isLoading ? (
-    // <button
-    //   onClick={async () => {
-    //     initConversation(
-    //       conversation_id,
-    //       user_roadmap_id,
-    //       node_title,
-    //       data.user_metadata.bio.language
-    //     );
-    //   }}
-    //   className=" border-text bg-text hover:bg-background text-background absolute top-[calc(50%-62px)] left-[calc(50%-90px)]  hover:text-text py-4 rounded-md font-semibold px-7 text-xl text-center border transition-color duration-150"
-    // >
-    //   Start learning
-    // </button>
-    <></>
+    <button
+      onClick={async () => {
+        initConversation(
+          conversation_id,
+          user_roadmap_id,
+          node_title,
+          data.user_metadata.bio.language
+        );
+      }}
+      className=" border-text bg-text hover:bg-background text-background absolute top-[calc(50%-62px)] left-[calc(50%-90px)]  hover:text-text py-4 rounded-md font-semibold px-7 text-xl text-center border transition-color duration-150"
+    >
+      Start learning
+    </button>
   ) : (
     <div className="border-text w-[180px] bg-text text-background absolute top-[calc(50%-62px)] left-[calc(50%-90px)] py-4 rounded-md px-7 text-xl border flex items-center justify-center">
       <div className="h-[40px] aspect-square border-2 border-background/60 border-t-background animate-spin rounded-full py-2" />
