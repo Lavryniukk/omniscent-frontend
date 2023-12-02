7c76becd5a4dad8063f55e1705ce40404f645b11
Lavryniukk <leoyt109@gmail.com>
2023-12-02 13:26:15 +0200

    Minor chabge

--- app/(pages)/(main)/(home)/page.tsx
+++ app/(pages)/(main)/(home)/page.tsx
@@ -6,17 +6,13 @@ import { NextPage } from "next";
 // HomePage is a Next.js page component representing the main landing page of the application.
 // It includes the Title component, Featupres component, and Reviews component.
 
-let HomePage: NextPage = () => {
-  let { theme, setTheme } = useTheme();
+function Home() {
   return (
     <main className="mb-10 font-inter space-y-20 overflow-hidden h-fit bg-transparent mx-auto box-border w-full">
-      <HeroSection /> {/* Renders the title section of the landing page. */}
+      <HeroSection />
       <FeaturesSection />{" "}
-      {/* Renders the features section of the landing page. */}
-      {/* <ReviewsSection />{" "} */}
-      {/* Renders the reviews section of the landing page. */}
     </main>
   );
-};
+}
 
-export default HomePage;
+export default Home;
