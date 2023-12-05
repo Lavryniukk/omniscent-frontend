8e460592ba154ad0632261d9b7d3163094b2dd94
Lavryniukk <leoyt109@gmail.com>
2023-12-05 21:18:07 +0200

    Some magic stuff, completely mystic

--- app/(pages)/(main)/overview/page.tsx
+++ app/(pages)/(main)/overview/page.tsx
@@ -1,3 +1,5 @@
-export default function Overview(){
-	
+export default function Overview() {
+  return (
+    <div className=" max-w-10xl border h-screen mt-14 lg:p-10 bg-clip-content bg-secondary mx-auto"></div>
+  );
 }
--- app/modules/ConversationWindow/components/Messages/components/Message.tsx
+++ app/modules/ConversationWindow/components/Messages/components/Message.tsx
@@ -1,71 +1,63 @@
-"use client";
-import React, { useEffect, useRef } from "react";
-import DOMPurify from "dompurify";
-import hljs from "highlight.js";
-import { useTheme } from "@/app/shared/providers/ThemeProvider";
+import React, { useEffect, useRef } from 'react';
+import ReactMarkdown from 'react-markdown';
+import hljs from 'highlight.js';
+import { useTheme } from '@/app/shared/providers/ThemeProvider';
 
-export default function Message({
-  role,
-  content,
-}: {
-  role: "user" | "assistant" | "system";
+interface MessageProps {
+  role: 'user' | 'system';
   content: string;
-}) {
-  const contentRef = useRef<HTMLDivElement>(null);
+}
 
+export default function Message({ role, content }: MessageProps) {
+  const contentRef = useRef<HTMLDivElement>(null);
   const { theme } = useTheme();
 
+  // Dynamically load highlight.js styles based on the theme
   useEffect(() => {
     const loadHighlightStyles = async () => {
-      if (theme === "dark") {
-        require("highlight.js/styles/github.css");
+      if (theme === 'dark') {
+        require('highlight.js/styles/github.css');
       } else {
-        require("highlight.js/styles/github-dark.css");
-      }
-      if (contentRef.current) {
-        contentRef.current.querySelectorAll("pre code").forEach((block) => {
-          hljs.highlightBlock(block as HTMLElement);
-        });
+        require('highlight.js/styles/github-dark.css');
       }
     };
 
     loadHighlightStyles();
-  }, [content, theme]);
+  }, [theme]);
 
-  const createMarkup = (htmlContent: string) => {
-    // First highlight the code, then sanitize
-    const div = document.createElement("div");
-    div.innerHTML = htmlContent;
-    div.querySelectorAll("pre code").forEach((block) => {
-      hljs.highlightBlock(block as HTMLElement);
-    });
-    const highlightedHtml = div.innerHTML;
-    const sanitizedContent = DOMPurify.sanitize(highlightedHtml);
-    return { __html: sanitizedContent };
-  };
+  // Apply syntax highlighting
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block as HTMLElement);
+      });
+    }
+  }, [content]);
+
+  // Conditional classNames
+  const wrapperClass = `text-text px-5 w-full mx-auto select-text flex-col text-md ${
+    role === 'system' ? 'hidden' : ''
+  } flex justify-start items-center gap-3 p-2 ${
+    role === 'user' ? 'bg-background' : 'bg-secondary'
+  }`;
+
+  const contentClass = 'max-w-[1000px] flex flex-col items-start w-full space-y-3';
+  const avatarClass = 'flex my-2 items-center justify-center';
+  const roleText = role === 'user' ? 'You' : 'AI Teacher';
 
   return (
-    <div
-      className={`text-text px-5 w-full mx-auto select-text flex-col text-md ${
-        role === "system" ? "hidden" : ""
-      } flex justify-start items-center gap-3 p-2 ${
-        role === "user" ? "bg-background" : "bg-secondary"
-      }`}
-    >
-      <div
-        className={`max-w-[1000px] flex flex-col items-start w-full space-y-3`}
-      >
-        <div className="flex my-2 items-center justify-center">
+    <div className={wrapperClass}>
+      <div className={contentClass}>
+        <div className={avatarClass}>
           <div className="w-[20px] lg:w-[32px] aspect-square border bg-accent rounded-full" />
-          <p className="ml-1 text-accent">
-            {role === "user" ? "You" : "AI Teacher"}
-          </p>
+          <p className="ml-1 text-accent">{roleText}</p>
         </div>
         <article
-          dangerouslySetInnerHTML={createMarkup(content)}
           className="mt-[5px] break-words text-lg w-full font-inter chat-output"
           ref={contentRef}
-        />
+        >
+          <ReactMarkdown>{content}</ReactMarkdown>
+        </article>
       </div>
     </div>
   );
