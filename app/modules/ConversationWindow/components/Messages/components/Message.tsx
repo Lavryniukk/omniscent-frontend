aeb2e1d0aa17daa481f0d93daa76094e59b48700
Lavryniukk <leoyt109@gmail.com>
2023-12-07 19:48:39 +0200

    fix

--- app/modules/ConversationWindow/components/Messages/components/Message.tsx
+++ app/modules/ConversationWindow/components/Messages/components/Message.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from 'react';
-import ReactMarkdown from 'react-markdown';
-import hljs from 'highlight.js';
-import { useTheme } from '@/app/shared/providers/ThemeProvider';
+"use effect";
+import React, { useEffect, useRef } from "react";
+import ReactMarkdown from "react-markdown";
+import hljs from "highlight.js";
+import { useTheme } from "@/app/shared/providers/ThemeProvider";
 
 interface MessageProps {
-  role: 'user' | 'system' | 'assistant';
+  role: "user" | "system" | "assistant";
   content: string;
 }
 
@@ -15,10 +16,10 @@ export default function Message({ role, content }: MessageProps) {
   // Dynamically load highlight.js styles based on the theme
   useEffect(() => {
     const loadHighlightStyles = async () => {
-      if (theme === 'dark') {
-        require('highlight.js/styles/github.css');
+      if (theme === "dark") {
+        require("highlight.js/styles/github.css");
       } else {
-        require('highlight.js/styles/github-dark.css');
+        require("highlight.js/styles/github-dark.css");
       }
     };
 
@@ -28,7 +29,7 @@ export default function Message({ role, content }: MessageProps) {
   // Apply syntax highlighting
   useEffect(() => {
     if (contentRef.current) {
-      contentRef.current.querySelectorAll('pre code').forEach((block) => {
+      contentRef.current.querySelectorAll("pre code").forEach((block) => {
         hljs.highlightBlock(block as HTMLElement);
       });
     }
@@ -36,14 +37,15 @@ export default function Message({ role, content }: MessageProps) {
 
   // Conditional classNames
   const wrapperClass = `text-text px-5 w-full mx-auto select-text flex-col text-md ${
-    role === 'system' ? 'hidden' : ''
+    role === "system" ? "hidden" : ""
   } flex justify-start items-center gap-3 p-2 ${
-    role === 'user' ? 'bg-background' : 'bg-secondary'
+    role === "user" ? "bg-background" : "bg-secondary"
   }`;
 
-  const contentClass = 'max-w-[1000px] flex flex-col items-start w-full space-y-3';
-  const avatarClass = 'flex my-2 items-center justify-center';
-  const roleText = role === 'user' ? 'You' : 'AI Teacher';
+  const contentClass =
+    "max-w-[1000px] flex flex-col items-start w-full space-y-3";
+  const avatarClass = "flex my-2 items-center justify-center";
+  const roleText = role === "user" ? "You" : "AI Teacher";
 
   return (
     <div className={wrapperClass}>
