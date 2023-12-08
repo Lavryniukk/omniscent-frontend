b0a1be2c212c1008d2fb86ccc2a0bd94c7055f05
Ivan Larin <n.larivanko@gmail.com>
2023-12-08 19:22:02 +0200

    fix

--- app/(pages)/workspace/page.tsx
+++ app/(pages)/workspace/page.tsx
@@ -5,14 +5,14 @@ import { NavigationButton } from "@/app/shared/components/buttons";
 import { UserProjects } from "@/app/modules";
 function WorkspacePage() {
   return (
-    <div className="flex h-screen border-accent items-center pt-28 sm:pt-0">
+    <div className="flex flex-col h-screen border-accent items-center justify-center pt-28 sm:pt-0">
       <NavigationButton href={"/"} title={"Home"} />
       <div className="flex flex-col py-10 w-min items-center min-w-[350px] gap-10 mx-auto">
-        <div className="border-2 bg-secondary p-5 gap-5 border-accent min-w-[350px] w-full rounded-lg flex flex-col items-start">
+        <div className="border-2 bg-secondary p-5 gap-5 border-accent min-w-[350px] w-8/12 sm:w-full rounded-lg flex flex-col items-start">
           <h1 className="text-2xl text-text font-inter font-bold italic text-left">
             Hint
           </h1>
-          <p className="text-lg text-text  font-normal">
+          <p className="text-lg text-text font-normal">
             This is your workspace.
             <br /> Here you will find all your existing projects. Project is a
             goal, something you want to learn. It can be anything from simple
