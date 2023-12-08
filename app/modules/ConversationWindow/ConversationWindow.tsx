1417ea63c15fec215dfd7cf3de080e0704180bcd
Ivan Larin <n.larivanko@gmail.com>
2023-12-08 19:47:57 +0200

    bf

--- app/modules/ConversationWindow/ConversationWindow.tsx
+++ app/modules/ConversationWindow/ConversationWindow.tsx
@@ -14,7 +14,7 @@ export default function ConversationWindow({
   const { conversation, selectConversation, tech } = useConversationStorage();
 
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`); 
 
   window.addEventListener("resize", () => {
     let vh = window.innerHeight * 0.01;
@@ -24,7 +24,7 @@ export default function ConversationWindow({
 
   return (
     <div
-      className={`w-full  flex items-center  flex-1  flex-col h-full  border-accent bg-secondary relative overflow-hidden `}
+      className={`w-full flex items-center flex-1 flex-col h-full border-accent bg-secondary relative overflow-hidden `}
     >
       <div className="w-full rounded-b-lg top-0 left-0 text-text tracking-widest py-4 flex items-center justify-center text-xl font-bold text-center bg-background">
         {conversation?.node_title}
