c3d7373f1b2d7af12d61f863dd5b3746c716c9e3
Lavryniukk <leoyt109@gmail.com>
2023-12-08 17:34:18 +0200

    Fix with non-rendering components

--- app/shared/components/buttons/NavigationButton.tsx
+++ app/shared/components/buttons/NavigationButton.tsx
@@ -4,13 +4,13 @@ import { MdOutlineArrowBack } from "react-icons/md";
 
 const NavigationButton = ({ href, title }: { href: string; title: string }) => {
   return (
-    <Link
+    <a
       href={href}
       className="flex items-center justify-center p-2  w-fit text-accent fixed left-10 top-10 hover:opacity-80"
     >
       <MdOutlineArrowBack />
       To {title}
-    </Link>
+    </a>
   );
 };
 
