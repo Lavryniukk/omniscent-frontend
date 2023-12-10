cfb30fc5f75ddc6eddfb1132632418160b75ab33
Lavryniukk <leoyt109@gmail.com>
2023-12-10 15:06:06 +0200

    fix

--- app/modules/NewProjectForm/components/FormSubmit/FormSubmit.tsx
+++ app/modules/NewProjectForm/components/FormSubmit/FormSubmit.tsx
@@ -2,8 +2,8 @@ export default function FormSubmit() {
   return (
     <button
       type="submit"
-      className={`text-text text-xl font-medium px-5 py-4 box-border border-text transition-colors duration-200 border w-3/4 mx-auto rounded-md 
-        hover:text-background hover:bg-text
+      className={`text-background bg-text text-xl font-medium px-5 py-4 box-border border-text transition-colors duration-200 border w-3/4 mx-auto rounded-md 
+        hover:text-text hover:bg-background
      `}
     >
       Create
