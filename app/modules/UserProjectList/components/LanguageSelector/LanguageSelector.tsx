fe0047a462e6bd2770e9f0154fb0589f4292d32e
Ivan Larin <n.larivanko@gmail.com>
2023-12-17 23:42:18 +0200

    added default language

--- app/modules/UserProjectList/components/LanguageSelector/LanguageSelector.tsx
+++ app/modules/UserProjectList/components/LanguageSelector/LanguageSelector.tsx
@@ -1,24 +1,35 @@
 "use client";
 import Skeleton from "@/app/UI/loading/Skeleton/Skeleton";
 import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function LanguageSelector() {
   const { user, isLoaded } = useUser();
   let language = user?.unsafeMetadata.language as string | undefined;
 
+  const setLanguage = async (value: string) => {
+    await user?.update({
+      unsafeMetadata: { language: value },
+    });
+  };
+
+  useEffect(() => {
+    if (!language) {
+      setLanguage("english");
+    }
+  }, []);
+
   return (
     <div className="absolute top-4 right-4 flex gap-1 justify-center items-center">
       <label className="text-sm text-text/60">
         Preferred learning language
-      </label>  
+      </label>
       {isLoaded ? (
         <select
           className="outline-none bg-transparent w-fit text-text/60"
           defaultValue={language ?? "english"}
           onChange={async (e) => {
-            await user?.update({
-              unsafeMetadata: { language: e.target.value },
-            });
+            await setLanguage(e.target.value);
           }}
         >
           <option value="english" className="bg-background text-text/60">
