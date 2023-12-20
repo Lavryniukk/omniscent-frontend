6a75b31c0c9b73f4b81434ed3a3f6da22124d5fe
Ivan Larin <n.larivanko@gmail.com>
2023-12-20 22:27:31 +0200

    added blog artcile

--- app/(pages)/(main)/blog/page.tsx
+++ app/(pages)/(main)/blog/page.tsx
@@ -2,15 +2,35 @@ import Link from "next/link";
 
 export default function Blog() {
   return (
-    <div className=" w-full max-w-10xl items-center flex flex-col h-screen pt-14 mx-auto">
+    <div className="w-full max-w-10xl items-center flex flex-col h-screen pt-14 mx-auto">
       <h1 className="text-5xl mt-20 text-text font-inter text-center font-bold ">
         Here you will find our latest updates
       </h1>
-      <div className="w-full flex items-center justify-center mt-20">
-        <div className="w-[350px] p-5 relative border-accent h-[350px] border rounded-lg">
+      <div className="w-full flex items-center justify-center mt-20 gap-4">
+        <div className="w-[400px] p-5 relative border-accent h-[400px] border rounded-lg">
+          <Link
+            href="/blog/v0.2.0"
+            className="bottom-5 hover:opacity-70 left-1/4 rounded-lg border absolute text-text flex items-center justify-center w-1/2 py-2"
+          >
+            Read more
+          </Link>
+          <p className="text-accent ">Decembre 20th 2023</p>
+          <h1 className="text-2xl text-text ">Cleverize v0.2.0</h1>
+          <p className="text-accent mt-4">
+            {`We are excited to introduce Cleverize, version V0.2.0, 
+            where we've shifted our focus towards programming languages!`}
+          </p>
+          <ul className="text-text mt-4">
+            <li>-Gradually improved programming language support</li>
+            <li>-Cutted out framework support (temporarily)</li>
+            <li>-Cutted out professions support (temporarily)</li>
+          </ul>
+        </div>
+
+        <div className="w-[400px] p-5 relative border-accent h-[400px] border rounded-lg">
           <Link
             href="/blog/v0.1.0"
-            className="bottom-5 hover:opacity-70 left-1/4 rounded-lg  border absolute text-text flex items-center justify-center w-1/2 py-2"
+            className="bottom-5 hover:opacity-70 left-1/4 rounded-lg border absolute text-text flex items-center justify-center w-1/2 py-2"
           >
             Read more
           </Link>
@@ -21,8 +41,8 @@ export default function Blog() {
             (MVP).
           </p>
           <ul className="text-text mt-4">
-            <li>-Roadmaps(customizable learning path)</li>
-            <li>-Conversations(learning chat with ai)</li>
+            <li>-Roadmaps (customizable learning path)</li>
+            <li>-Conversations (learning chat with ai)</li>
           </ul>
         </div>
       </div>
