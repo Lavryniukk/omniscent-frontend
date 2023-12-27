8a3d146d082cff0868439adea76c031e79893f63
Lavryniukk <leoyt109@gmail.com>
2023-12-27 15:06:57 +0200

    removed profile page and module

--- app/(pages)/(main)/profile/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileModule } from "@/app/modules";
-
-export default function Profile({ params }: { params: { id: string } }) {
-  return (
-    <>
-      <ProfileModule id={params.id} />
-    </>
-  );
-}
--- app/modules/Profile/components/Achievements/Achievements.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Achievements() {
-  return (
-    <div className="rounded-lg">
-      <h2 className="text-text text-xl sm:text-lg mb-2">Achievements</h2>
-      <div className="h-20 bg-background border border-secondary rounded w-3/4 sm:w-full flex "></div>
-    </div>
-  );
-}
