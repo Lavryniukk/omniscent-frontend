82b0039f6c0cdbcc182b80bc0375e20ab62a28a2
Ivan Larin <n.larivanko@gmail.com>
2023-12-28 16:25:57 +0200

    updated responsiveness for memberships page

--- app/UI/static/Memberships/PricingCards/components/ProCard/ProCard.tsx
+++ app/UI/static/Memberships/PricingCards/components/ProCard/ProCard.tsx
@@ -11,7 +11,7 @@ let ProCard = () => {
   // write function handle click based on func below
 
   return (
-    <div className="observe sm:-translate-x-5 mx-auto sm:m-0 z-20 shadow-[0px_0px_30px_0px_rgb(var(--text))] sm:shadow-[10px_10px_35px_0px_rgb(var(--text))] border-t border-r duration-500 transition delay-75 w-[350px] md:w-[450px] p-8 relative bg-background h-fit bg-opacity-90 rounded-xl border-secondary">
+    <div className="observe md:-translate-x-5 mx-auto sm:m-0 z-20 shadow-[0px_0px_30px_0px_rgb(var(--text))] md:shadow-[10px_10px_35px_0px_rgb(var(--text))] border-t border-r duration-500 transition delay-75 w-[350px] sm:w-[450px] md:w-[400px] lg:w-[450px]  p-8 relative bg-background h-fit bg-opacity-90 rounded-xl border-secondary">
       <h1 className="mb-2 text-2xl font-light text-left text-text">Advanced</h1>
       <h1 className="text-4xl font-bold text-left  text-text">
         $14.99
