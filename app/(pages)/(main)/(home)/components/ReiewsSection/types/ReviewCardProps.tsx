c6fd5b88423acc88e62d07c2cd2812bf18971615
Lavryniukk <leoyt109@gmail.com>
2024-02-05 16:43:32 +0200

    Update package.json and middleware.ts, delete ReviewCardProps.tsx, and modify Navigation.tsx, page.tsx, and FeaturesSection.tsx

--- app/(pages)/(main)/(home)/components/ReiewsSection/types/ReviewCardProps.tsx
+++
@@ -1,8 +0,0 @@
-export default interface ReviewCard {
-  name: string; // The reviewer's name
-  rating: number; // The rating given by the reviewer
-  nickname: string; // The reviewer's nickname or username
-  content: string; // The content of the review
-  imageUrl: string; // URL to the reviewer's profile picture
-  className?: string;
-}
--- app/(pages)/(main)/(home)/page.tsx
+++ app/(pages)/(main)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import FeaturesSection from "@/app/(pages)/(main)/(home)/components/FeaturesSection/FeaturesSection";
 import HeroSection from "@/app/(pages)/(main)/(home)/components/HeroSection/HeroSection";
+import FeedbackSection from "./components/FeedbackSection/FeedbackSection";
 
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
     <main className="mb-10 mt-16 max-w-10xl font-inter overflow-hidden flex flex-col justify-start h-fit overflow-y-auto bg-transparent mx-auto box-border w-full">
       <HeroSection />
       <FeaturesSection />
+      <FeedbackSection />
     </main>
   );
 }
