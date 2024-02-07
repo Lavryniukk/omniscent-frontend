289e0221101aecef0832012edee145c3ba0f3d30
Ivan Larin <n.larivanko@gmail.com>
2024-02-07 23:46:14 +0200

    initiial commit

--- app/(pages)/(main)/(home)/components/HeroSection/HeroSection.tsx
+++ app/(pages)/(main)/(home)/components/HeroSection/HeroSection.tsx
@@ -2,9 +2,11 @@
 import { useTheme } from "next-themes";
 import Title from "./components/Title";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 let HeroSection = () => {
   const { resolvedTheme } = useTheme();
+
   let src;
 
   switch (resolvedTheme) {
@@ -20,21 +22,27 @@ let HeroSection = () => {
       break;
   }
   return (
-    <div className="  h-fit py-20  relative overflow-hidden flex flex-col items-center">
+    <div className="h-fit py-20 relative overflow-hidden flex flex-col items-center">
       <Title />
-      <picture className="w-[98%]  p-0.5 relative flex items-center overflow-hidden justify-center  border-azure-950 rounded md:rounded-2xl  md:w-[80%] ">
-        <Image
-          src={src}
-          alt="A cleverize conversation example"
-          className="w-full   shadow-2xl rounded-[14px] "
-          width={1920}
-          height={1040}
-        />
-        <div className=" w-[50%] h-[15%] absolute  bg-azure-300 dark:bg-azure-700 left-0  -bottom-[15%] rounded-full blur-[120px]" />
-        <div className=" w-[50%] h-[15%]  absolute bg-azure-300 dark:bg-azure-700 right-0 -bottom-[15%] rounded-full blur-[120px]" />
+      <div className="w-[98%] p-0.5 home-image md:w-[80%]">
+        <motion.picture
+          initial={{ rotateX: -5, translateZ: -200 }}
+          whileInView={{ rotateX: 0, translateZ: 0 }}
+          viewport={{ once: true, amount: 0.7 }}
+          className={`w-full relative flex items-center overflow-hidden justify-center border-azure-950 rounded md:rounded-2xl `}
+        >
+          <Image
+            src={src}
+            alt="A cleverize conversation example"
+            width={1920}
+            height={1040}
+          />
+          <div className="w-[50%] h-[15%] absolute bg-azure-300 dark:bg-azure-700 left-0 -bottom-[15%] rounded-full blur-[120px]" />
+          <div className="w-[50%] h-[15%] absolute bg-azure-300 dark:bg-azure-700 right-0 -bottom-[15%] rounded-full blur-[120px]" />
 
-        <div className="absolute  bg-gradient-to-b from-transparent rounded-2xl to-azure-200 dark:to-azure-800 -z-10 w-full h-full" />
-      </picture>
+          <div className="absolute bg-gradient-to-b from-transparent rounded-2xl to-azure-200 dark:to-azure-800 -z-10 w-full h-full" />
+        </motion.picture>
+      </div>
       {/* <div className="absolute bottom-0 bg-gradient-to-t from-background  to-transparent w-full h-32 z-[20]" /> */}
       {/* <MovingGradients /> */}
     </div>
