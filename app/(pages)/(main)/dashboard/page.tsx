018c3173a18671f6839920b7d0d6d20f584dae72
Lavryniukk <leoyt109@gmail.com>
2024-02-08 16:57:19 +0200

    Remove dashboard page component

--- app/(pages)/(main)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardProjects from "@/app/modules/DashboardProjects/DashboardProjects";
-
-export default function Dashboard() {
-  return (
-    <div className="max-w-10xl h-fit  flex sm:px-10 mx-auto">
-      <DashboardProjects />
-    </div>
-  );
-}
