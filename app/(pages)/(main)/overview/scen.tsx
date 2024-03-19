b13813d2eefb3411ea2e27f11abc4d90d9dcfb21
Lavryniuk Lev <leoyt109@gmail.com>
2024-03-19 03:14:23 +0200

    fucks sake

---
+++ app/(pages)/(main)/overview/scen.tsx
@@ -0,0 +1,5 @@
+`
+1. Title: Learn whatever you want, whenever you want.
+2. Subtitle: Flexibility
+3. Description: Since all learning material comes from well-trained AI-models, you can learn anything related to coding out there. You can learn at your own pace, and at your own time. The power of the sun, in the palm of your hand!
+`;
