d8cd8299602f7b910af749f74637ac8df2594ec0
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-05-29 23:27:33 +0200

    !UNSTABLE: working on hljs and markdown bug

--- app/widgets/lesson/ui/Message.tsx
+++ app/widgets/lesson/ui/Message.tsx
@@ -6,20 +6,21 @@ import hljs from "highlight.js";
 import "highlight.js/styles/tokyo-night-dark.css";
 
 interface MessageProps {
-  role: "user" | "system" | "assistant";
+  role: "user" | "system" | "model";
   content: string;
 }
 
 export default function Message({ role, content }: MessageProps) {
   const contentRef = useRef<HTMLDivElement>(null);
-
+  //TODO - markdown and hljs does not seem to work during streaming, but after refreshing the page it works
   useEffect(() => {
+    console.log("effect triggered with contenxt", content);
     if (contentRef.current) {
       contentRef.current.querySelectorAll("pre code").forEach((block) => {
         try {
-          block && block.removeAttribute("data-highlighted");
-          block && hljs.highlightElement(block as HTMLElement);
-        } catch (e) {} // Very important error handling (not a joke) ;)
+          // Highlight the block
+          hljs.highlightElement(block as HTMLElement);
+        } catch {}
       });
     }
   }, [content]);
@@ -35,24 +36,20 @@ export default function Message({ role, content }: MessageProps) {
           <div
             className={`w-[20px] lg:w-[32px] shadow-xl aspect-square border ${role == "user" ? "" : ""}  rounded-full`}
           />
-          <p className="ml-1 ">
-            {role === "user" ? "You" : "AI Teacher"}
-          </p>
+          <p className="ml-1 ">{role === "user" ? "You" : "AI Teacher"}</p>
         </div>
         <article
           className="mt-[5px] break-words  font-medium  text-base leading-[28px] w-full font-inter chat-output"
           ref={contentRef}
         >
-          {role === "assistant" ? (
+          {role === "model" ? (
             content === "isLoading" ? (
               <div className="border-2 w-10 h-10 rounded-full border-t-foreground animate-spin aspect-square" />
             ) : (
               <Markdown>{content}</Markdown>
             )
           ) : (
-            <div className=" font-medium ">
-              {content}
-            </div>
+            <div className="font-medium">{content}</div>
           )}
         </article>
       </div>
